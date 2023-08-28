ece80a7f2cb3784c7a4e2796c3bce523a84c8041
grantcolley <grantcolley@yahoo.com>
2023-08-28 21:44:04 +0100

    Atlas.Data.Access initial check-in

---
+++ src/Atlas.Data.Access/Data/WeatherForecastRepository.cs
@@ -0,0 +1,28 @@
+﻿using Atlas.Core.Models;
+using Atlas.Data.Access.Interfaces;
+
+namespace Atlas.Data.Access.Data
+{
+    public class WeatherForecastRepository : IWeatherForecastRepository
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+            "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts(CancellationToken cancellationToken)
+        {
+            var rng = new Random();
+            WeatherForecast[] weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return await Task.FromResult(weatherForecasts);
+        }
+    }
+}
---
+++ src/Atlas.Data.Access/Interfaces/IWeatherForecastRepository.cs
@@ -0,0 +1,9 @@
+﻿using Atlas.Core.Models;
+
+namespace Atlas.Data.Access.Interfaces
+{
+    public interface IWeatherForecastRepository
+    {
+        Task<IEnumerable<WeatherForecast>> GetWeatherForecasts(CancellationToken cancellationToken);
+    }
+}
