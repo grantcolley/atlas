2bf5eac5bf39b43fec57680c2848476fa485c652
grantcolley <grantcolley@yahoo.com>
2023-08-30 15:15:59 +0100

    Create Auth.cs

---
+++ src/Atlas.Core/Constants/Auth.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlas.Core.Constants
+{
+    public static class Auth
+    {
+        public const string ATLAS_USER = "atlas-user";
+        public const string ROLE = "role";
+        public const string USER = "user";
+    }
+}
