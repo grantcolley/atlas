dd8d631d682c4c7856754d19a49b9be0dd09bdce
grantcolley <grantcolley@yahoo.com>
2023-08-31 16:21:23 +0100

    Initial check-in for Atlas.Blazor.Server

---
+++ src/Atlas.Blazor.Server/Pages/Login.cshtml.cs
@@ -0,0 +1,32 @@
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace Atlas.Blazor.Server.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public async Task OnGetAsync(string redirectUri)
+        {
+            if (string.IsNullOrWhiteSpace(redirectUri))
+            {
+                redirectUri = Url.Content("~/");
+            }
+
+            if (HttpContext.User.Identity != null
+                && HttpContext.User.Identity.IsAuthenticated)
+            {
+                Response.Redirect(redirectUri);
+            }
+
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithRedirectUri(redirectUri)
+                .Build();
+
+            await HttpContext.ChallengeAsync(
+                Auth0Constants.AuthenticationScheme,
+                authenticationProperties).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Atlas.Blazor.Server/Program.cs
@@ -0,0 +1,84 @@
+using Atlas.Blazor.Components.Cache;
+using Atlas.Blazor.Components.Interfaces;
+using Atlas.Blazor.Components.Services;
+using Atlas.Core.Models;
+using Atlas.Requests.API;
+using Atlas.Requests.Interfaces;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MudBlazor.Services;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services
+    .AddAuth0WebAppAuthentication(Auth0Constants.AuthenticationScheme, options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Domain"] ?? throw new NullReferenceException("Auth0:Domain");
+        options.ClientId = builder.Configuration["Auth0:ClientId"] ?? throw new NullReferenceException("Auth0:ClientId");
+        options.ClientSecret = builder.Configuration["Auth0:ClientSecret"] ?? throw new NullReferenceException("Auth0:ClientSecret");
+        options.ResponseType = "code";
+    }).WithAccessToken(options =>
+    {
+        options.Audience = builder.Configuration["Auth0:Audience"] ?? throw new NullReferenceException("Auth0:Audience");
+    });
+
+builder.Services.AddHttpClient("atlas-api", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:44320");
+});
+
+builder.Services.AddScoped<TokenProvider>();
+builder.Services.AddSingleton<IAtlasCache, AtlasCache>();
+builder.Services.AddSingleton<IStateNotificationService, StateNotificationService>();
+builder.Services.AddTransient<IDialogService, DialogService>();
+
+builder.Services.AddTransient<IModuleRequests, ModuleRequests>(sp =>
+{
+    var tokenProvider = sp.GetRequiredService<TokenProvider>();
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient("atlas-api");
+    return new ModuleRequests(httpClient, tokenProvider);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapBlazorHub();
+
+app.MapFallbackToPage("/_Host");
+
+app.Run();
