ce76f884f32a313ffc29fda3b2ee06ed41aacea3
grantcolley <grantcolley@yahoo.com>
2023-09-02 13:40:31 +0100

    Create Audit.cs

---
+++ src/Atlas.Data.Access/Model/Audit.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Atlas.Data.Access.Model
+{
+    public class Audit
+    {
+        public int Id { get; set; }
+        public string? ClrType { get; set; }
+        public string? TableName { get; set; }
+        public string? Action { get; set; }
+        public string? EntityId { get; set; }
+        public string? OldValues { get; set; }
+        public string? NewValues { get; set; }
+        public string? User { get; set; }
+        public DateTime DateTime { get; set; }
+
+        [NotMapped]
+        public List<PropertyEntry> TemporaryProperties { get; } = new List<PropertyEntry>();
+
+        [NotMapped]
+        public Dictionary<string, object> OldValuesDictionary { get; } = new Dictionary<string, object>();
+
+        [NotMapped]
+        public Dictionary<string, object> NewValuesDictionary { get; } = new Dictionary<string, object>();
+    }
+}
