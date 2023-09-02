6d6bd5f0438643d8d9a028c17065a12d3e531027
grantcolley <grantcolley@yahoo.com>
2023-09-02 13:40:26 +0100

    Create DataBase

---
+++ src/Atlas.Data.Access/Data/DataBase.cs
@@ -0,0 +1,75 @@
+﻿using Atlas.Core.Models;
+using Atlas.Data.Access.Context;
+using Atlas.Data.Access.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Atlas.Data.Access.Data
+{
+    public abstract class DataBase<T> : IData
+    {
+        protected readonly ApplicationDbContext applicationDbContext;
+        protected readonly ILogger<T> logger;
+        private bool disposedValue;
+
+        protected string? User;
+
+        protected DataBase(ApplicationDbContext applicationDbContext, ILogger<T> logger)
+        {
+            this.applicationDbContext = applicationDbContext;
+            this.logger = logger;
+        }
+
+        public void SetUser(string user)
+        {
+            User = user;
+
+            applicationDbContext.SetUser(user);
+        }
+
+        public async Task<bool> IsAuthorisedAsync(string claim, string permission)
+        {
+            return await applicationDbContext.Users
+                .AsNoTracking()
+                .AnyAsync(
+                u => u.Email.Equals(claim)
+                && (u.Permissions.Any(p => p.Name.Equals(permission))
+                || u.Roles.SelectMany(r => r.Permissions).Any(p => p.Name.Equals(permission))))
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Authorisation> GetAuthorisationAsync(string claim)
+        {
+            var user = await applicationDbContext.Users
+                .AsNoTracking()
+                .Include(u => u.Permissions)
+                .Include(u => u.Roles)
+                .ThenInclude(r => r.Permissions)
+                .FirstOrDefaultAsync(u => u.Email.Equals(claim))
+                .ConfigureAwait(false);
+
+            var permissionSet = user.GetUserPermissionSet();
+
+            return new Authorisation { User = claim, Permissions = permissionSet };
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    applicationDbContext.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/Atlas.Data.Access/Interfaces/IData.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Core.Models;
+
+namespace Atlas.Data.Access.Interfaces
+{
+    public interface IData : IDisposable
+    {
+        void SetUser(string user);
+        Task<Authorisation> GetAuthorisationAsync(string claim);
+        Task<bool> IsAuthorisedAsync(string claim, string permission);
+    }
+}
