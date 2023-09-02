6e1b8392e8808fb153cae97b109e9afe20162849
grantcolley <grantcolley@yahoo.com>
2023-09-02 13:39:18 +0100

    Create Authorisation.cs

---
+++ src/Atlas.Core/Models/Authorisation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Atlas.Core.Models
+{
+    public class Authorisation
+    {
+        public Authorisation()
+        {
+            Permissions = new List<string>();
+        }
+
+        public string? User { get; set; }
+        public List<string> Permissions { get; set; }
+    }
+}
