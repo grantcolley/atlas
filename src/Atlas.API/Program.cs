a84d3c7cea581396f5e26da9805eb142d0f4514f
grantcolley <grantcolley@yahoo.com>
2023-09-03 21:26:03 +0100

    Update Program.cs

--- src/Atlas.API/Program.cs
+++ src/Atlas.API/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("local",
         builder =>
-            builder.WithOrigins("https://localhost:7225", "https://localhost:7226")
+            builder.WithOrigins("https://localhost:44400", "https://localhost:44410")
                    .AllowAnyHeader());
 });
 
