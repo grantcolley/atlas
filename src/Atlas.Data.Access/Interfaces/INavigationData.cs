9214a1adfa50d518964cb2ed84a6e186a8af5c33
grantcolley <grantcolley@yahoo.com>
2023-09-05 19:16:47 +0100

    Create INavigationData.cs

---
+++ src/Atlas.Data.Access/Interfaces/INavigationData.cs
@@ -0,0 +1,9 @@
+﻿using Atlas.Core.Models;
+
+namespace Atlas.Data.Access.Interfaces
+{
+    public interface INavigationData
+    {
+        Task<IEnumerable<Module>> GetNavigationClaimsAsync(string claim, CancellationToken cancellationToken);
+    }
+}
