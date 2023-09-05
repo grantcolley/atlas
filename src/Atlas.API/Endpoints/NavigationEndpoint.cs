1aac071ecb25f00990ca8f5ffa4d578175be121c
grantcolley <grantcolley@yahoo.com>
2023-09-05 19:16:52 +0100

    Create NavigationEndpoint.cs

---
+++ src/Atlas.API/Endpoints/NavigationEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Atlas.Core.Models;
+using Atlas.Data.Access.Interfaces;
+
+namespace Atlas.API.Endpoints
+{
+    internal static class NavigationEndpoint
+    {
+        internal static async Task<IResult> GetNavigationClaims(INavigationData navigationData, CancellationToken cancellationToken)
+        {
+            try
+            {
+                IEnumerable<Module>? modules = await navigationData.GetNavigationClaimsAsync("", cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(modules);
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from navigationData.GetNavigationClaimsAsync(token)
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
