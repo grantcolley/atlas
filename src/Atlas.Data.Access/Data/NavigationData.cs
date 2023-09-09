cbac136f7119c5810822277e3bb00e8cea21bb6d
grantcolley <grantcolley@yahoo.com>
2023-09-09 18:15:17 +0100

    Update NavigationData.cs

--- src/Atlas.Data.Access/Data/NavigationData.cs
+++ src/Atlas.Data.Access/Data/NavigationData.cs
@@ -15,35 +15,50 @@ namespace Atlas.Data.Access.Data
 
         public async Task<IEnumerable<Module>?> GetNavigationClaimsAsync(string claim, CancellationToken cancellationToken)
         {
-            var user = await applicationDbContext.Users
+            if(string.IsNullOrWhiteSpace(claim))
+            {
+                return default;
+            }
+
+            if(applicationDbContext == null)
+            {
+                throw new NullReferenceException(nameof(applicationDbContext));
+            }
+
+            User? user = await applicationDbContext.Users
                 .Include(u => u.Permissions)
                 .Include(u => u.Roles)
                 .ThenInclude(r => r.Permissions)
                 .AsNoTracking()
-                .FirstOrDefaultAsync(u => u.Email.Equals(claim))
+                .FirstOrDefaultAsync(u => u.Email != null && u.Email.Equals(claim), cancellationToken)
                 .ConfigureAwait(false);
 
-            var userPermissions = user.Permissions.Select(p => p.Name).ToList();
+            if(user == null) 
+            {
+                return default;
+            }
 
-            var rolePermissions = user.Roles
+            List<string?> userPermissions = user.Permissions.Select(p => p.Name).ToList();
+
+            List<string?> rolePermissions = user.Roles
                 .SelectMany(r => r.Permissions)
                 .Select(p => p.Name)
                 .ToList();
 
             userPermissions.AddRange(rolePermissions);
 
-            var permissions = userPermissions.Distinct().ToList();
+            List<string?> permissions = userPermissions.Distinct().ToList();
 
-            var modules = await applicationDbContext.Modules
+            List<Module> modules = await applicationDbContext.Modules
                 .AsNoTracking()
                 .Include(m => m.Categories.OrderBy(c => c.Order))
                 .ThenInclude(c => c.MenuItems.OrderBy(mu => mu.Order))
                 .Where(m => permissions.Contains(m.Permission))
                 .OrderBy(m => m.Order)
-                .ToListAsync()
+                .ToListAsync(cancellationToken)
                 .ConfigureAwait(false);
 
-            var permittedModules = modules
+            List<Module> permittedModules = modules
                 .Where(m => m.IsPermitted(permissions))
                 .ToList();
 
