3a079ef478e36808524280eee6c1f2416d24bc07
grantcolley <grantcolley@yahoo.com>
2023-09-09 17:19:35 +0100

    Create AuthorisationService.cs

---
+++ src/Atlas.API/Services/AuthorisationService.cs
@@ -0,0 +1,38 @@
+﻿using Atlas.API.Interfaces;
+using Atlas.Core.Models;
+using Atlas.Data.Access.Interfaces;
+using System.Security.Claims;
+
+namespace Atlas.API.Services
+{
+    public class AuthorisationService : IAuthorisationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAuthorisationData _authorisationData;
+        private readonly Claim? _claim;
+
+        public AuthorisationService(IHttpContextAccessor httpContextAccessor, IAuthorisationData authorisationData)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new Exception(nameof(HttpContextAccessor));
+            _authorisationData = authorisationData ?? throw new ArgumentNullException(nameof(authorisationData));
+
+            if (_httpContextAccessor.HttpContext == null)
+            {
+                throw new NullReferenceException(nameof(_httpContextAccessor.HttpContext));
+            }
+
+            if (_httpContextAccessor.HttpContext.User.Identity == null)
+            {
+                throw new NullReferenceException(nameof(_httpContextAccessor.HttpContext.User.Identity));
+            }
+
+            ClaimsIdentity claimsIdentity = (ClaimsIdentity)_httpContextAccessor.HttpContext.User.Identity;
+            _claim = claimsIdentity.FindFirst(ClaimTypes.Email);
+        }
+
+        public Task<Authorisation?> GetAuthorisationAsync()
+        {
+            return _authorisationData.GetAuthorisationAsync(_claim?.Value ?? string.Empty);
+        }
+    }
+}
