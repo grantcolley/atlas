008c45934ad2988b96d1d9dc7adb154371f236e6
grantcolley <grantcolley@yahoo.com>
2023-09-09 17:12:47 +0100

    Update DataBase.cs

--- src/Atlas.Data.Access/Data/DataBase.cs
+++ src/Atlas.Data.Access/Data/DataBase.cs
@@ -1,56 +1,18 @@
-﻿using Atlas.Core.Models;
-using Atlas.Data.Access.Context;
-using Atlas.Data.Access.Interfaces;
-using Microsoft.EntityFrameworkCore;
+﻿using Atlas.Data.Access.Context;
 using Microsoft.Extensions.Logging;
 
 namespace Atlas.Data.Access.Data
 {
-    public abstract class DataBase<T> : IData
+    public abstract class DataBase<T> : IDisposable
     {
         protected readonly ApplicationDbContext applicationDbContext;
         protected readonly ILogger<T> logger;
         private bool disposedValue;
 
-        protected string? User;
-
         protected DataBase(ApplicationDbContext applicationDbContext, ILogger<T> logger)
         {
             this.applicationDbContext = applicationDbContext;
             this.logger = logger;
-        }
-
-        public void SetUser(string user)
-        {
-            User = user;
-
-            applicationDbContext.SetUser(user);
-        }
-
-        public async Task<bool> IsAuthorisedAsync(string claim, string permission)
-        {
-            return await applicationDbContext.Users
-                .AsNoTracking()
-                .AnyAsync(
-                u => u.Email.Equals(claim)
-                && (u.Permissions.Any(p => p.Name.Equals(permission))
-                || u.Roles.SelectMany(r => r.Permissions).Any(p => p.Name.Equals(permission))))
-                .ConfigureAwait(false);
-        }
-
-        public async Task<Authorisation> GetAuthorisationAsync(string claim)
-        {
-            var user = await applicationDbContext.Users
-                .AsNoTracking()
-                .Include(u => u.Permissions)
-                .Include(u => u.Roles)
-                .ThenInclude(r => r.Permissions)
-                .FirstOrDefaultAsync(u => u.Email.Equals(claim))
-                .ConfigureAwait(false);
-
-            var permissionSet = user.GetUserPermissionSet();
-
-            return new Authorisation { User = claim, Permissions = permissionSet };
         }
 
         protected virtual void Dispose(bool disposing)
