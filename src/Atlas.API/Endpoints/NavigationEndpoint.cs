9172a2236b0a79e5180328cb0c9c6b0d82c8418e
grantcolley <grantcolley@yahoo.com>
2023-09-09 17:19:38 +0100

    Update NavigationEndpoint.cs

--- src/Atlas.API/Endpoints/NavigationEndpoint.cs
+++ src/Atlas.API/Endpoints/NavigationEndpoint.cs
@@ -1,15 +1,25 @@
-﻿using Atlas.Core.Models;
+﻿using Atlas.API.Interfaces;
+using Atlas.Core.Models;
 using Atlas.Data.Access.Interfaces;
 
 namespace Atlas.API.Endpoints
 {
     internal static class NavigationEndpoint
     {
-        internal static async Task<IResult> GetNavigationClaims(INavigationData navigationData, CancellationToken cancellationToken)
+        internal static async Task<IResult> GetClaimModules(IAuthorisationService authorisationService, INavigationData navigationData, CancellationToken cancellationToken)
         {
             try
             {
-                IEnumerable<Module>? modules = await navigationData.GetNavigationClaimsAsync("", cancellationToken)
+                var authorisation = await authorisationService.GetAuthorisationAsync()
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || string.IsNullOrWhiteSpace(authorisation.User))
+                {
+                    return Results.Unauthorized();
+                }
+
+                IEnumerable<Module>? modules = await navigationData.GetNavigationClaimsAsync(authorisation.User, cancellationToken)
                     .ConfigureAwait(false);
 
                 return Results.Ok(modules);
