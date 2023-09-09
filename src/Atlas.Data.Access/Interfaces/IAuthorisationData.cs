a176adb4cb3b428e24c678f6ed6467347fa4e349
grantcolley <grantcolley@yahoo.com>
2023-09-09 17:12:37 +0100

    Create IAuthorisationData.cs

---
+++ src/Atlas.Data.Access/Interfaces/IAuthorisationData.cs
@@ -0,0 +1,9 @@
+﻿using Atlas.Core.Models;
+
+namespace Atlas.Data.Access.Interfaces
+{
+    public interface IAuthorisationData
+    {
+        Task<Authorisation?> GetAuthorisationAsync(string user);
+    }
+}
