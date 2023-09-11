62fd81fc3242dbd394f6a98880c9997f3efdc44b
grantcolley <grantcolley@yahoo.com>
2023-09-11 08:47:45 +0100

    Create ClaimService.cs

---
+++ src/Atlas.API/Services/ClaimService.cs
@@ -0,0 +1,33 @@
+﻿using Atlas.API.Interfaces;
+using System.Security.Claims;
+
+namespace Atlas.API.Services
+{
+    public class ClaimService : IClaimService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly Claim? _claim;
+
+        public ClaimService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new Exception(nameof(HttpContextAccessor));
+
+            if (_httpContextAccessor.HttpContext == null)
+            {
+                throw new NullReferenceException(nameof(_httpContextAccessor.HttpContext));
+            }
+
+            if (_httpContextAccessor.HttpContext.User.Identity == null)
+            {
+                throw new NullReferenceException(nameof(_httpContextAccessor.HttpContext.User.Identity));
+            }
+
+            _claim = ((ClaimsIdentity)_httpContextAccessor.HttpContext.User.Identity).FindFirst(ClaimTypes.Email);
+        }
+
+        public string? GetClaim()
+        {
+            return _claim?.Value;
+        }
+    }
+}
