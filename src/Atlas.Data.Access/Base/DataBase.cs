91f41b1db7db85682e9bf38962d3577f945c6ef5
grantcolley <grantcolley@yahoo.com>
2023-09-11 08:26:37 +0100

    Create DataBase.cs

---
+++ src/Atlas.Data.Access/Base/DataBase.cs
@@ -0,0 +1,37 @@
+﻿using Atlas.Data.Access.Context;
+using Microsoft.Extensions.Logging;
+
+namespace Atlas.Data.Access.Base
+{
+    public abstract class DataBase<T> : IDisposable
+    {
+        protected readonly ApplicationDbContext _applicationDbContext;
+        protected readonly ILogger<T> _logger;
+        private bool _disposedValue;
+
+        protected DataBase(ApplicationDbContext applicationDbContext, ILogger<T> logger)
+        {
+            _applicationDbContext = applicationDbContext ?? throw new ArgumentNullException(nameof(applicationDbContext));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _applicationDbContext.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
