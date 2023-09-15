d6fc2ea5faf02f37c44228f66b5053de94081ba1
grantcolley <grantcolley@yahoo.com>
2023-09-15 21:01:14 +0100

    Create AtlasComponentArgsBase.cs

---
+++ src/Atlas.Blazor.Components/Base/AtlasComponentArgsBase.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Core.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Blazor.Components.Base
+{
+    public class AtlasComponentArgsBase : AtlasComponentBase
+    {
+        [Parameter]
+        public ComponentArgs? ComponentArgs { get; set; }
+    }
+}
