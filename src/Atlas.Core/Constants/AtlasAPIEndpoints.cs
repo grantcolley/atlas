1f021571ec93294cbdadb1bc98a9ed94b18be761
grantcolley <grantcolley@yahoo.com>
2023-09-15 21:00:14 +0100

    Create AtlasAPIEndpoints.cs

---
+++ src/Atlas.Core/Constants/AtlasAPIEndpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace Atlas.Core.Constants
+{
+    public static class AtlasAPIEndpoints
+    {
+        public const string CLAIM_MODULES = "claimmodules";
+        public const string COMPONENT_ARGS = "componentargs";
+    }
+}
