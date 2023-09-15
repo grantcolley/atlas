bf677f82efc9d3c430c08f6a5edf9a0d1b50ab14
grantcolley <grantcolley@yahoo.com>
2023-09-15 20:59:35 +0100

    Add IComponentArgsData and ComponentArgsData

---
+++ src/Atlas.Data.Access/Interfaces/IComponentArgsData.cs
@@ -0,0 +1,9 @@
+﻿using Atlas.Core.Models;
+
+namespace Atlas.Data.Access.Interfaces
+{
+    public interface IComponentArgsData : IAuthorisationData
+    {
+        Task<ComponentArgs?> GetComponentArgsAsync(string? componentCode, CancellationToken cancellationToken);
+    }
+}
