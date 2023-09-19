e3fedd41dcd4280b72d5f1b7f34f359a03e0038e
grantcolley <grantcolley@yahoo.com>
2023-09-19 13:23:37 +0100

    Update NavigationEndpoint.cs

--- src/Atlas.API/Endpoints/NavigationEndpoint.cs
+++ src/Atlas.API/Endpoints/NavigationEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using Atlas.API.Interfaces;
+using Atlas.Core.Constants;
 using Atlas.Core.Models;
 using Atlas.Data.Access.Interfaces;
 
@@ -26,7 +27,34 @@ namespace Atlas.API.Endpoints
             }
             catch (Exception)
             {
-                // Exceptions thrown from navigationData.GetNavigationClaimsAsync(token)
+                // Exceptions thrown from navigationData.GetNavigationClaimsAsync(claim)
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> GetModules(INavigationData navigationData, IClaimService claimService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Authorisation? authorisation = await navigationData.GetAuthorisationAsync(claimService.GetClaim())
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.ADMIN))
+                {
+                    return Results.Unauthorized();
+                }
+
+                IEnumerable<Module>? modules = await navigationData.GetModulesAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(modules);
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from navigationData.GetModulesAsync()
                 // have already been logged so simply return Status500InternalServerError.
 
                 return Results.StatusCode(StatusCodes.Status500InternalServerError);
