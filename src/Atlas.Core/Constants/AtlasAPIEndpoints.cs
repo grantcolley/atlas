fe048142c6256ccb5d8de2a020e38d3d69b96d6b
grantcolley <grantcolley@yahoo.com>
2023-09-19 13:23:29 +0100

    Update AtlasAPIEndpoints.cs

--- src/Atlas.Core/Constants/AtlasAPIEndpoints.cs
+++ src/Atlas.Core/Constants/AtlasAPIEndpoints.cs
@@ -4,5 +4,6 @@
     {
         public const string CLAIM_MODULES = "claimmodules";
         public const string COMPONENT_ARGS = "componentargs";
+        public const string GET_MODULES = "getmodules";
     }
 }
