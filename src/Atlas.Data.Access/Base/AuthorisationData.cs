8451711488db1751d680230f10d5ed1d5c9e27bd
grantcolley <grantcolley@yahoo.com>
2023-09-19 08:21:53 +0100

    Create AuthorisationData.cs

---
+++ src/Atlas.Data.Access/Base/AuthorisationData.cs
@@ -0,0 +1,50 @@
+﻿using Atlas.Core.Models;
+using Atlas.Data.Access.Base;
+using Atlas.Data.Access.Context;
+using Atlas.Data.Access.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Atlas.Data.Access.Data
+{
+    public abstract class AuthorisationData<T> : DataBase<T>, IAuthorisationData
+    {
+        public AuthorisationData(ApplicationDbContext applicationDbContext, ILogger<T> logger)
+            : base(applicationDbContext, logger)
+        {
+        }
+
+        public async Task<Authorisation?> GetAuthorisationAsync(string? claim)
+        {
+            if(string.IsNullOrWhiteSpace(claim))
+            {
+                return default;
+            }
+
+            var user = await _applicationDbContext.Users
+                .AsNoTracking()
+                .Include(u => u.Permissions)
+                .Include(u => u.Roles)
+                .ThenInclude(r => r.Permissions)
+                .FirstOrDefaultAsync(u => u.Email != null && u.Email.Equals(claim))
+                .ConfigureAwait(false);
+
+            if(user != null
+                && user.Email != null) 
+            {
+                SetUser(user.Email);
+
+                var permissionSet = user.GetUserPermissionSet();
+
+                return new Authorisation { User = claim, Permissions = permissionSet };
+            }
+
+            return default;
+        }
+
+        private void SetUser(string user)
+        {
+            _applicationDbContext.SetUser(user);
+        }
+    }
+}
