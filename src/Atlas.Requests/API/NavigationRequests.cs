dbc953097d2417e365706edbb5385b91b2fb8216
grantcolley <grantcolley@yahoo.com>
2023-09-21 08:56:15 +0100

    Create INavigationRequests and NavigationRequests

---
+++ src/Atlas.Requests/API/NavigationRequests.cs
@@ -0,0 +1,30 @@
+﻿using Atlas.Core.Constants;
+using Atlas.Core.Models;
+using Atlas.Requests.Base;
+using Atlas.Requests.Interfaces;
+using System.Text.Json;
+
+namespace Atlas.Requests.API
+{
+    public class NavigationRequests : RequestBase, INavigationRequests
+    {
+        public NavigationRequests(HttpClient httpClient)
+            : base(httpClient)
+        {
+        }
+
+        public NavigationRequests(HttpClient httpClient, TokenProvider tokenProvider)
+            : base(httpClient, tokenProvider)
+        {
+        }
+
+        public async Task<IEnumerable<Module>?> GetClaimModulesAsync()
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<Module>?>
+                (await _httpClient.GetStreamAsync(AtlasAPIEndpoints.CLAIM_MODULES)
+                .ConfigureAwait(false),
+                new JsonSerializerOptions(JsonSerializerDefaults.Web))
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Atlas.Requests/Interfaces/INavigationRequests.cs
@@ -0,0 +1,9 @@
+﻿using Atlas.Core.Models;
+
+namespace Atlas.Requests.Interfaces
+{
+    public interface INavigationRequests
+    {
+        Task<IEnumerable<Module>?> GetClaimModulesAsync();
+    }
+}
