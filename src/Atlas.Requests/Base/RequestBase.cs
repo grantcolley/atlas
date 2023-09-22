fea99f74af002aca50aae326f8c9b2ceb6f28243
grantcolley <grantcolley@yahoo.com>
2023-09-22 07:15:57 +0100

    Update RequestBase.cs

--- src/Atlas.Requests/Base/RequestBase.cs
+++ src/Atlas.Requests/Base/RequestBase.cs
@@ -1,26 +1,55 @@
 ﻿using Atlas.Core.Models;
+using Atlas.Requests.Interfaces;
+using Atlas.Requests.Model;
+using System.Text.Json;
 
 namespace Atlas.Requests.Base
 {
     public abstract class RequestBase
     {
         protected readonly HttpClient _httpClient;
-        protected readonly TokenProvider? _tokenProvider;
 
         protected RequestBase(HttpClient httpClient, TokenProvider tokenProvider)
             : this(httpClient)
         {
-            _tokenProvider = tokenProvider ?? throw new ArgumentNullException(nameof(tokenProvider));
+            if (tokenProvider == null)
+            {
+                throw new ArgumentNullException(nameof(tokenProvider));
+            }
 
             if (_httpClient.DefaultRequestHeaders.Authorization == null)
             {
-                _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_tokenProvider.AccessToken}");
+                _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {tokenProvider.AccessToken}");
             }
         }
 
         protected RequestBase(HttpClient httpClient)
         {
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        protected static async Task<IResponse<T>> GetResponseAsync<T>(HttpResponseMessage httpResponseMessage)
+        {
+            var response = new Response<T>
+            {
+                IsSuccess = httpResponseMessage.IsSuccessStatusCode,
+                Message = httpResponseMessage.ReasonPhrase
+            };
+
+            if (response.IsSuccess)
+            {
+                response.Result = await JsonSerializer.DeserializeAsync<T>
+                    (await httpResponseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false),
+                        new JsonSerializerOptions(JsonSerializerDefaults.Web)).ConfigureAwait(false);
+            }
+            else
+            {
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+                string? debug = httpResponseMessage.Content.ReadAsStringAsync().Result;
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+            }
+
+            return response;
         }
     }
 }
