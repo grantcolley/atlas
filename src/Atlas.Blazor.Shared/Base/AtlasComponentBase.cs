1a946e947cfd6c4124d62d9ed2dc2f1680b33ef1
grantcolley <grantcolley@yahoo.com>
2023-09-22 19:29:45 +0100

    Update AtlasComponentBase.cs

--- src/Atlas.Blazor.Shared/Base/AtlasComponentBase.cs
+++ src/Atlas.Blazor.Shared/Base/AtlasComponentBase.cs
@@ -1,8 +1,6 @@
 ﻿using Atlas.Blazor.Shared.Constants;
 using Atlas.Blazor.Shared.Interfaces;
 using Atlas.Blazor.Shared.Models;
-using Atlas.Core.Models;
-using Atlas.Requests.Interfaces;
 using Microsoft.AspNetCore.Components;
 
 namespace Atlas.Blazor.Shared.Base
@@ -14,41 +12,6 @@ namespace Atlas.Blazor.Shared.Base
 
         [Inject]
         public IStateNotificationService? StateNotificationService { get; set; }
-
-        [Inject]
-        public IComponentArgsRequests? ComponentArgsRequests { get; set; }
-
-        [Parameter]
-        public string? ComponentCode { get; set; }
-
-        protected ComponentArgs? _componentArgs = default;
-
-        protected override async Task OnParametersSetAsync()
-        {
-            await base.OnParametersSetAsync().ConfigureAwait(false);
-
-            if (!string.IsNullOrWhiteSpace(ComponentCode)
-                && NavigationManager != null
-                && StateNotificationService != null
-                && ComponentArgsRequests != null)
-            {
-                _componentArgs = await ComponentArgsRequests.GetComponentArgsAsync(ComponentCode)
-                    .ConfigureAwait(false);
-
-                if (_componentArgs != null)
-                {
-                    var breadcrumb = new Breadcrumb
-                    {
-                        Text = _componentArgs.DisplayName,
-                        Href = NavigationManager.Uri.Remove(0, NavigationManager.BaseUri.Length - 1),
-                        ResetAfterHome = _componentArgs.NavigateResetBreadcrumb
-                    };
-
-                    await StateNotificationService.NotifyStateHasChangedAsync(StateNotifications.BREADCRUMBS, breadcrumb)
-                        .ConfigureAwait(false);
-                }
-            }
-        }
 
         protected void RaiseAlert(string message)
         {
