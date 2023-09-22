bf12c4447b5f522c9c89b56f3b1dfb2214839030
grantcolley <grantcolley@yahoo.com>
2023-09-22 08:35:42 +0100

    Create AtlasGenericComponentBase.cs

---
+++ src/Atlas.Blazor.Shared/Base/AtlasGenericComponentBase.cs
@@ -0,0 +1,11 @@
+﻿using Atlas.Requests.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Blazor.Shared.Base
+{
+    public class AtlasGenericComponentBase : AtlasComponentArgsBase
+    {
+        [Inject]
+        public IGenericRequests? GenericRequests { get; set; }
+    }
+}
