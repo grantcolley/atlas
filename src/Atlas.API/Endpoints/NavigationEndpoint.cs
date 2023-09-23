6e396d1079a48218102b6ec22a7f48d4d8af355f
grantcolley <grantcolley@yahoo.com>
2023-09-23 07:20:51 +0100

    Update NavigationEndpoint.cs

--- src/Atlas.API/Endpoints/NavigationEndpoint.cs
+++ src/Atlas.API/Endpoints/NavigationEndpoint.cs
@@ -33,32 +33,5 @@ namespace Atlas.API.Endpoints
                 return Results.StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
-
-        internal static async Task<IResult> GetModules(INavigationData navigationData, IClaimService claimService, CancellationToken cancellationToken)
-        {
-            try
-            {
-                Authorisation? authorisation = await navigationData.GetAuthorisationAsync(claimService.GetClaim())
-                    .ConfigureAwait(false);
-
-                if (authorisation == null
-                    || !authorisation.HasPermission(Auth.ADMIN))
-                {
-                    return Results.Unauthorized();
-                }
-
-                IEnumerable<Module>? modules = await navigationData.GetModulesAsync(cancellationToken)
-                    .ConfigureAwait(false);
-
-                return Results.Ok(modules);
-            }
-            catch (Exception)
-            {
-                // Exceptions thrown from navigationData.GetModulesAsync()
-                // have already been logged so simply return Status500InternalServerError.
-
-                return Results.StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
     }
 }
