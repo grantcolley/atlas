545a7fafcca4d4cddca04198c2a965edcaf0d414
grantcolley <grantcolley@yahoo.com>
2023-09-25 15:25:15 +0100

    Update AdministrationEndpoints.cs

--- src/Atlas.API/Endpoints/AdministrationEndpoints.cs
+++ src/Atlas.API/Endpoints/AdministrationEndpoints.cs
@@ -2,6 +2,7 @@
 using Atlas.Core.Constants;
 using Atlas.Core.Models;
 using Atlas.Data.Access.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Atlas.API.Endpoints
 {
@@ -55,6 +56,87 @@ namespace Atlas.API.Endpoints
             catch (Exception)
             {
                 // Exceptions thrown from navigationData.GetModuleAsync()
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> CreateModule([FromBody] Module module, INavigationData navigationData, IClaimService claimService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Authorisation? authorisation = await navigationData.GetAuthorisationAsync(claimService.GetClaim())
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.ADMIN))
+                {
+                    return Results.Unauthorized();
+                }
+
+                Module? newModule = await navigationData.CreateModuleAsync(module, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(newModule);
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from navigationData.CreateModuleAsync()
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> UpdateModule([FromBody] Module module, INavigationData navigationData, IClaimService claimService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Authorisation? authorisation = await navigationData.GetAuthorisationAsync(claimService.GetClaim())
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.ADMIN))
+                {
+                    return Results.Unauthorized();
+                }
+
+                Module? updatedModule = await navigationData.UpdateModuleAsync(module, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(updatedModule);
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from navigationData.UpdateModuleAsync()
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> DeleteModule(int id, INavigationData navigationData, IClaimService claimService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Authorisation? authorisation = await navigationData.GetAuthorisationAsync(claimService.GetClaim())
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || !authorisation.HasPermission(Auth.ADMIN))
+                {
+                    return Results.Unauthorized();
+                }
+
+                int affectedRows = await navigationData.DeleteModuleAsync(id, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(affectedRows);
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from navigationData.DeleteModuleAsync()
                 // have already been logged so simply return Status500InternalServerError.
 
                 return Results.StatusCode(StatusCodes.Status500InternalServerError);
