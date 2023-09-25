3a2a4dbf05a895c77a0972f4180fbe2bad561226
grantcolley <grantcolley@yahoo.com>
2023-09-25 16:00:51 +0100

    Create ModuleView.razor

---
+++ src/Atlas.Blazor.Shared/Components/Admin/ModuleView.razor.cs
@@ -0,0 +1,24 @@
+﻿using Atlas.Blazor.Shared.Base;
+using Atlas.Core.Constants;
+
+namespace Atlas.Blazor.Shared.Components.Admin
+{
+    public abstract class ModuleViewBase : AtlasComponentArgsBase
+    {
+        protected string _identityField = "ModuleId";
+        protected string _titleField = "Name";
+        protected string _createEndpoint = AtlasAPIEndpoints.CREATE_MODULE;
+        protected string _updateEndpoint = AtlasAPIEndpoints.UPDATE_MODULE;
+        protected string _deleteEndpoint = AtlasAPIEndpoints.DELETE_MODULE;
+
+        protected override void OnInitialized()
+        {
+            if (ComponentArgs == null)
+            {
+                throw new ArgumentNullException(nameof(ComponentArgs));
+            }
+
+            base.OnInitialized();
+        }
+    }
+}
