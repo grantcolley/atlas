0c4985b198b2b94cf8097a97f2916a8382d512a5
grantcolley <grantcolley@yahoo.com>
2023-09-25 15:24:46 +0100

    Create EndpointMapper.cs

---
+++ src/Atlas.API/Extensions/EndpointMapper.cs
@@ -0,0 +1,18 @@
+﻿namespace Atlas.API.Extensions
+{
+    public static class EndpointMapper
+    {
+        public static WebApplication? MapEndpoints(this WebApplication app)
+        {
+            app.MapHealthChecks("/health");
+
+            app.MapGet("/error", () => Results.Problem());
+
+            app.MapAtlas();
+
+            app.MapAtlasAdministration();
+
+            return app;
+        }
+    }
+}
