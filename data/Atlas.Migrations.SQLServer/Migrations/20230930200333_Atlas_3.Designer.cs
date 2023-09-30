bad8dd86d2219acba967002b1986072da2f793ba
grantcolley <grantcolley@yahoo.com>
2023-09-30 21:32:21 +0100

    Update SQL Server migrations

--- data/Atlas.Migrations.SQLServer/Migrations/20230930200333_Atlas_3.Designer.cs
+++ data/Atlas.Migrations.SQLServer/Migrations/20230930200333_Atlas_3.Designer.cs
@@ -4,6 +4,7 @@ using Atlas.Data.Access.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Atlas.Migrations.SQLServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230930200333_Atlas_3")]
+    partial class Atlas_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
