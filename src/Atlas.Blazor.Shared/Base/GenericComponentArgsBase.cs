d5e18033576856b517b54f6407550fca700e5173
grantcolley <grantcolley@yahoo.com>
2023-09-30 22:42:16 +0100

    Update GenericComponentArgsBase.cs

--- src/Atlas.Blazor.Shared/Base/GenericComponentArgsBase.cs
+++ src/Atlas.Blazor.Shared/Base/GenericComponentArgsBase.cs
@@ -1,11 +1,15 @@
-﻿using Atlas.Requests.Interfaces;
+﻿using Atlas.Core.Models;
+using Atlas.Requests.Interfaces;
 using Microsoft.AspNetCore.Components;
 
 namespace Atlas.Blazor.Shared.Base
 {
-    public class GenericComponentArgsBase : ComponentArgsBase
+    public class GenericComponentArgsBase : AtlasComponentBase
     {
         [Inject]
         public IGenericRequests? GenericRequests { get; set; }
+
+        [Parameter]
+        public ComponentArgs? ComponentArgs { get; set; }
     }
 }
