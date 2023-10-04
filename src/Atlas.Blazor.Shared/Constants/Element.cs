7c9392431c9a77cb355a7b152c13af1db4d85b3e
grantcolley <grantcolley@yahoo.com>
2023-10-04 09:10:58 +0100

    Create Element.cs

---
+++ src/Atlas.Blazor.Shared/Constants/Element.cs
@@ -0,0 +1,8 @@
+﻿namespace Atlas.Blazor.Shared.Constants
+{
+    public static class Element
+    {
+        public const string Label = "Atlas.Blazor.Shared.Components.Mud.Label`1, Atlas.Blazor.Shared";
+        public const string Text = "Atlas.Blazor.Shared.Components.Mud.Text`1, Atlas.Blazor.Shared";
+    }
+}
