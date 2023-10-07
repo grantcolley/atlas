c3917d9fad287375f69cda7b59098053a667f129
grantcolley <grantcolley@yahoo.com>
2023-10-07 22:35:37 +0100

    Update AtlasConstants.cs

--- src/Atlas.Core/Constants/AtlasConstants.cs
+++ src/Atlas.Core/Constants/AtlasConstants.cs
@@ -3,5 +3,7 @@
     public static class AtlasConstants
     {
         public const string ATLAS_API = "atlas-api";
+        public const string THEME_DARK = "Dark";
+        public const string THEME_LIGHT = "Light";
     }
 }
