38b0b99dbf54b9db539b5fdce6f2e763305ad26f
grantcolley <grantcolley@yahoo.com>
2023-10-07 22:36:42 +0100

    Update AtlasEndpointMapper.cs

--- src/Atlas.API/Extensions/AtlasEndpointMapper.cs
+++ src/Atlas.API/Extensions/AtlasEndpointMapper.cs
@@ -8,10 +8,26 @@ namespace Atlas.API.Extensions
     {
         public static WebApplication? MapAtlas(this WebApplication app)
         {
-            app.MapGet($"/{AtlasAPIEndpoints.CLAIM_MODULES}", NavigationEndpoint.GetClaimModules)
+            app.MapGet($"/{AtlasAPIEndpoints.GET_CLAIM_MODULES}", UserEndpoint.GetClaimModules)
                 .WithOpenApi()
-                .WithName(AtlasAPIEndpoints.CLAIM_MODULES)
+                .WithName(AtlasAPIEndpoints.GET_CLAIM_MODULES)
                 .WithDescription("Gets the user's authorized modules")
+                .Produces<IEnumerable<Module>?>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapGet($"/{AtlasAPIEndpoints.GET_THEME}", UserEndpoint.GetTheme)
+                .WithOpenApi()
+                .WithName(AtlasAPIEndpoints.GET_THEME)
+                .WithDescription("Gets the user's theme preference")
+                .Produces<IEnumerable<Module>?>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
+
+            app.MapPost($"/{AtlasAPIEndpoints.SET_THEME}", UserEndpoint.SetTheme)
+                .WithOpenApi()
+                .WithName(AtlasAPIEndpoints.SET_THEME)
+                .WithDescription("Sets the user's theme preference")
                 .Produces<IEnumerable<Module>?>(StatusCodes.Status200OK)
                 .Produces(StatusCodes.Status500InternalServerError)
                 .RequireAuthorization(Auth.ATLAS_USER_CLAIM);
