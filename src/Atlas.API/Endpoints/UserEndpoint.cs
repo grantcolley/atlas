9d67b581c02cbe69f61ec353d480afe90537df0c
grantcolley <grantcolley@yahoo.com>
2023-10-07 22:36:38 +0100

    Create UserEndpoint.cs

---
+++ src/Atlas.API/Endpoints/UserEndpoint.cs
@@ -0,0 +1,89 @@
+﻿using Atlas.API.Interfaces;
+using Atlas.Core.Constants;
+using Atlas.Core.Models;
+using Atlas.Data.Access.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atlas.API.Endpoints
+{
+    internal static class UserEndpoint
+    {
+        internal static async Task<IResult> GetClaimModules(IUserData userData, IClaimService claimService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Authorisation? authorisation = await userData.GetAuthorisationAsync(claimService.GetClaim())
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || string.IsNullOrWhiteSpace(authorisation.User))
+                {
+                    return Results.Unauthorized();
+                }
+
+                IEnumerable<Module>? modules = await userData.GetNavigationClaimsAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok(modules);
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from userData.GetNavigationClaimsAsync(claim)
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> GetTheme(IUserData userData, IClaimService claimService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Authorisation? authorisation = await userData.GetAuthorisationAsync(claimService.GetClaim())
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || string.IsNullOrWhiteSpace(authorisation.User))
+                {
+                    return Results.Unauthorized();
+                }
+
+                return Results.Ok(authorisation.Theme);
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from userData.GetNavigationClaimsAsync(claim)
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        internal static async Task<IResult> SetTheme([FromBody] string theme, IUserData userData, IClaimService claimService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Authorisation? authorisation = await userData.GetAuthorisationAsync(claimService.GetClaim())
+                    .ConfigureAwait(false);
+
+                if (authorisation == null
+                    || string.IsNullOrWhiteSpace(authorisation.User))
+                {
+                    return Results.Unauthorized();
+                }
+
+                await userData.SetThemePreferenceAsync(theme, cancellationToken)
+                    .ConfigureAwait(false);
+
+                return Results.Ok();
+            }
+            catch (Exception)
+            {
+                // Exceptions thrown from userData.GetNavigationClaimsAsync(claim)
+                // have already been logged so simply return Status500InternalServerError.
+
+                return Results.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
