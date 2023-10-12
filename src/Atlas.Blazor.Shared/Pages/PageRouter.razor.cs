90da69f95ca0bdfbbe5990a973dcd4da6b44cf36
grantcolley <grantcolley@yahoo.com>
2023-10-12 21:11:13 +0100

    Update PageRouter.razor.cs

--- src/Atlas.Blazor.Shared/Pages/PageRouter.razor.cs
+++ src/Atlas.Blazor.Shared/Pages/PageRouter.razor.cs
@@ -39,12 +39,13 @@ namespace Atlas.Blazor.Shared.Pages
                         _componentArgs.ModelInstanceId = Id.Value;
                     }
 
-                    string idValue = Id.HasValue && Id.Value > 0 ? $" {Id.Value}" : string.Empty;
+                    string idValue = Id.HasValue && Id.Value > 0 ? $"{Id.Value}" : string.Empty;
 
                     var breadcrumb = new Breadcrumb
                     {
-                        Text = $"{_componentArgs.DisplayName}{idValue}",
-                        Href = NavigationManager.Uri.Remove(0, NavigationManager.BaseUri.Length - 1)
+                        Text = $"{_componentArgs.DisplayName} {idValue}",
+                        Href = NavigationManager.Uri.Remove(0, NavigationManager.BaseUri.Length - 1),
+                        BreadcrumbAction = BreadcrumbAction.Add
                     };
 
                     await StateNotificationService.NotifyStateHasChangedAsync(StateNotifications.BREADCRUMBS, breadcrumb)
