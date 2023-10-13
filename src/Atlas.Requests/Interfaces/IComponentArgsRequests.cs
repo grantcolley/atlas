e40ce43c929e60e760058f55eccb793579147230
grantcolley <grantcolley@yahoo.com>
2023-10-13 21:31:13 +0100

    Delete IComponentArgsRequests.cs

--- src/Atlas.Requests/Interfaces/IComponentArgsRequests.cs
+++
@@ -1,9 +0,0 @@
-﻿using Atlas.Core.Models;
-
-namespace Atlas.Requests.Interfaces
-{
-    public interface IComponentArgsRequests
-    {
-        Task<ComponentArgs?> GetComponentArgsAsync(string? componentCode);
-    }
-}
