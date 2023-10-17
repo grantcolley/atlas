1f43203e49cca379aecd8e4bb06f8abde1de2cc9
grantcolley <grantcolley@yahoo.com>
2023-10-17 13:59:24 +0100

    Create ElementParams.cs

---
+++ src/Atlas.Blazor.Shared/Constants/ElementParams.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlas.Blazor.Shared.Constants
+{
+    public static class ElementParams
+    {
+        public const string MAX = "Max";
+        public const string MAX_LENGTH = "MaxLength";
+        public const string MIN = "Min";
+    }
+}
