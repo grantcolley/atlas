fe4dd2a89523702ff15ff54f7f7d74134546f53c
grantcolley <grantcolley@yahoo.com>
2023-10-18 20:37:25 +0100

    Update OptionsArgExtensions.cs

--- src/Atlas.Core/Extensions/OptionsArgExtensions.cs
+++ src/Atlas.Core/Extensions/OptionsArgExtensions.cs
@@ -6,19 +6,19 @@ namespace Atlas.Core.Extensions
 {
     public static class OptionsArgExtensions
     {
-        public static OptionsArg? FirstOptionsArgDefault(this IEnumerable<OptionsArg> args, string name)
+        public static OptionsArg? FirstOptionsArgDefault(this IEnumerable<OptionsArg> optionsArgs, string name)
         {
-            return args.FirstOrDefault(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name));
+            return optionsArgs.FirstOrDefault(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name));
         }
 
-        public static string? FirstOptionsArgValue(this IEnumerable<OptionsArg> args, string name)
+        public static string? FirstOptionsArgValue(this IEnumerable<OptionsArg> optionsArgs, string name)
         {
-            return args.First(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name))?.Value;
+            return optionsArgs.First(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name))?.Value;
         }
 
-        public static bool HasOptionsArg(this IEnumerable<OptionsArg> args, string name)
+        public static bool HasOptionsArg(this IEnumerable<OptionsArg> optionsArgs, string name)
         {
-            return args.Any(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name));
+            return optionsArgs.Any(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name));
         }
     }
 }
