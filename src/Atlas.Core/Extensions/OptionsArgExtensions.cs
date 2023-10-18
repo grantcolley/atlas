5efb5771aef9b2c80b903c4984cd7175ad229164
grantcolley <grantcolley@yahoo.com>
2023-10-18 20:10:47 +0100

    Create OptionsArgExtensions.cs

---
+++ src/Atlas.Core/Extensions/OptionsArgExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Atlas.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atlas.Core.Extensions
+{
+    public static class OptionsArgExtensions
+    {
+        public static OptionsArg? FirstOptionsArgDefault(this IEnumerable<OptionsArg> args, string name)
+        {
+            return args.FirstOrDefault(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name));
+        }
+
+        public static string? FirstOptionsArgValue(this IEnumerable<OptionsArg> args, string name)
+        {
+            return args.First(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name))?.Value;
+        }
+
+        public static bool HasOptionsArg(this IEnumerable<OptionsArg> args, string name)
+        {
+            return args.Any(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name));
+        }
+    }
+}
