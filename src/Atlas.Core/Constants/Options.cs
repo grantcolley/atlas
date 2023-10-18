6fb57a154a62d7e7a48ea1c7b6fa2b216eded6bd
grantcolley <grantcolley@yahoo.com>
2023-10-18 20:10:43 +0100

    Create Options.cs

---
+++ src/Atlas.Core/Constants/Options.cs
@@ -0,0 +1,10 @@
+﻿namespace Atlas.Core.Constants
+{
+    public static class Options
+    {
+        public const string OPTIONS_CODE = "OptionsCode";
+        public const string PERMISSIONS_OPTION_ITEMS = "Permissions";
+        public const string MODULES_OPTION_ITEMS = "Modules";
+        public const string CATEGORIES_OPTION_ITEMS = "Categories";
+    }
+}
