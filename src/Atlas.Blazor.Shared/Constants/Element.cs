abaa439a7de4cf795fece4726ee93e0d8149f137
grantcolley <grantcolley@yahoo.com>
2023-10-19 08:51:36 +0100

    Update Element.cs

--- src/Atlas.Blazor.Shared/Constants/Element.cs
+++ src/Atlas.Blazor.Shared/Constants/Element.cs
@@ -2,6 +2,7 @@
 {
     public static class Element
     {
+        public const string Dropdown = "Atlas.Blazor.Shared.Components.Mud.Dropdown`1, Atlas.Blazor.Shared";
         public const string Integer = "Atlas.Blazor.Shared.Components.Mud.Integer`1, Atlas.Blazor.Shared";
         public const string Label = "Atlas.Blazor.Shared.Components.Mud.Label`1, Atlas.Blazor.Shared";
         public const string Text = "Atlas.Blazor.Shared.Components.Mud.Text`1, Atlas.Blazor.Shared";
