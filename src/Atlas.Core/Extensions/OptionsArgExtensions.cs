73b748806275e96724faffa91d05de6b36fbccc7
grantcolley <grantcolley@yahoo.com>
2023-10-19 08:51:47 +0100

    Update OptionsArgExtensions.cs

--- src/Atlas.Core/Extensions/OptionsArgExtensions.cs
+++ src/Atlas.Core/Extensions/OptionsArgExtensions.cs
@@ -6,6 +6,18 @@ namespace Atlas.Core.Extensions
 {
     public static class OptionsArgExtensions
     {
+        public static IEnumerable<OptionsArg> ToOptionsArgs(this Dictionary<string, string> parameters)
+        { 
+            List<OptionsArg> optionsArgs = new List<OptionsArg>();
+
+            foreach (KeyValuePair<string, string> kvp in parameters) 
+            {
+                optionsArgs.Add(new OptionsArg { Name = kvp.Key, Value = kvp.Value });
+            }
+
+            return optionsArgs;
+        }
+
         public static OptionsArg? FirstOptionsArgDefault(this IEnumerable<OptionsArg> optionsArgs, string name)
         {
             return optionsArgs.FirstOrDefault(a => !string.IsNullOrWhiteSpace(a.Name) && a.Name.Equals(name));
