a9f6a1106c9d0199fce538bf0cb525421df1860c
grantcolley <grantcolley@yahoo.com>
2023-10-26 13:54:06 +0100

    Update ElementParams.cs

--- src/Atlas.Blazor.Shared/Constants/ElementParams.cs
+++ src/Atlas.Blazor.Shared/Constants/ElementParams.cs
@@ -2,7 +2,7 @@
 {
     public static class ElementParams
     {
-        public const string DISPLAY_FIELD = "DisplayField";
+        public const string GENERIC_MODEL_DISPLAY_FIELD = "GenericDisplayField";
         public const string GENERIC_COMPONENT_NAME = "GenericComponentName";
         public const string IS_NUMERIC_ID = "IsNumericId";
         public const string MAX = "Max";
