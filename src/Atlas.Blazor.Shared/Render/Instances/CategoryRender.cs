0cec7a7feedfc018c788dd2da8346460a8446035
grantcolley <grantcolley@yahoo.com>
2023-10-26 20:51:39 +0100

    Update CategoryRender.cs

--- src/Atlas.Blazor.Shared/Render/Instances/CategoryRender.cs
+++ src/Atlas.Blazor.Shared/Render/Instances/CategoryRender.cs
@@ -8,37 +8,38 @@ namespace Atlas.Blazor.Shared.Render.Instances
     {
         public override void Configure()
         {
-            RenderProperty(m => m.CategoryId)
+            RenderProperty(c => c.CategoryId)
                 .RenderAs(Element.Label)
                 .WithLabel("Category Id")
                 .WithTooltip("Category Id")
                 .RenderOrder(1);
 
-            RenderProperty(m => m.Name)
+            RenderProperty(c => c.Name)
                 .RenderAs(Element.Text)
                 .WithLabel("Name")
                 .WithTooltip("Name")
                 .RenderOrder(2);
 
-            RenderProperty(m => m.Order)
+            RenderProperty(c => c.Order)
                 .RenderAs(Element.Integer)
                 .WithLabel("Order")
                 .WithTooltip("Order")
                 .RenderOrder(3);
 
-            RenderProperty(m => m.Icon)
+            RenderProperty(c => c.Icon)
                 .RenderAs(Element.DropdownIcon)
                 .WithLabel("Icon")
                 .WithTooltip("Icon")
                 .RenderOrder(4);
 
-            RenderProperty(m => m.Module)
-                .RenderAsGenericComponent(Element.DropdownGeneric)
+            RenderGenericProperty(c => c.Module)
+                .RenderAsDropdownGeneric("Name")
                 .WithLabel("Module")
                 .WithTooltip("Module")
+                .WithParameters(new Dictionary<string, string> { { Options.OPTIONS_CODE, Options.MODULES_OPTION_ITEMS } })
                 .RenderOrder(5);
 
-            RenderProperty(m => m.Permission)
+            RenderProperty(c => c.Permission)
                 .RenderAs(Element.Dropdown)
                 .WithLabel("Permission")
                 .WithTooltip("Permission")
