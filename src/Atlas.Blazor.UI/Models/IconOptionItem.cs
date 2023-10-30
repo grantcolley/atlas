a8f93975b0e3624732eabbaeceba625e56277633
grantcolley <grantcolley@yahoo.com>
2023-10-30 07:40:09 +0000

    Update IconOptionItem.cs

--- src/Atlas.Blazor.UI/Models/IconOptionItem.cs
+++ src/Atlas.Blazor.UI/Models/IconOptionItem.cs
@@ -1,9 +1,42 @@
-﻿namespace Atlas.Blazor.UI.Models
+﻿using System.Xml.Linq;
+
+namespace Atlas.Blazor.UI.Models
 {
     public class IconOptionItem
     {
         public string? Id { get; set; }
         public string? Display { get; set; }
         public string? Icon { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+
+            if (obj is not IconOptionItem iconOptionItem)
+            {
+                return false;
+            }
+
+            return iconOptionItem?.Display == this.Display;
+        }
+
+        public override int GetHashCode()
+        {
+            if (string.IsNullOrWhiteSpace(Display))
+            {
+                return "empty".GetHashCode();
+            }
+
+            return Display.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return Display;
+        }
     }
 }
