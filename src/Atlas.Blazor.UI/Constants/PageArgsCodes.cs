0e446c3152423672e2eb05901ce570373c2382eb
grantcolley <grantcolley@yahoo.com>
2023-10-30 07:21:27 +0000

    Update PageArgsCodes.cs

--- src/Atlas.Blazor.UI/Constants/PageArgsCodes.cs
+++ src/Atlas.Blazor.UI/Constants/PageArgsCodes.cs
@@ -8,5 +8,11 @@
         public const string CATEGORIES = "Categories";
         public const string MENU_ITEM = "MenuItem";
         public const string MENU_ITEMS = "MenuItems";
+        public const string USER = "User";
+        public const string USERS = "Users";
+        public const string ROLE = "Role";
+        public const string ROLES = "Roles";
+        public const string PERMISSION = "Permission";
+        public const string PERMISSIONS = "Permissions";
     }
 }
