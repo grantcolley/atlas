3f7582d4485b2e7ff452af6e6f176e4febf62432
grantcolley <grantcolley@yahoo.com>
2023-11-01 21:13:39 +0000

    Create DynamicTypeCreator.cs

---
+++ src/Atlas.Core/Dynamic/DynamicTypeCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Atlas.Core.Dynamic
+{
+    public static class DynamicTypeCreator<T>
+    {
+        private static readonly Func<T> creator =
+            Expression.Lambda<Func<T>>(Expression.New(typeof(T).GetConstructor(Type.EmptyTypes) ?? throw new InvalidOperationException(nameof(T))))
+            .Compile();
+
+        public static T Create()
+        {
+            return creator();
+        }
+
+        public static IEnumerable<T> CreateList()
+        {
+            var listType = typeof(List<>);
+            var genericListType = listType.MakeGenericType(typeof(T));
+            var instance = Activator.CreateInstance(genericListType);
+            if (instance == null) throw new InvalidOperationException(nameof(instance));
+            return (IEnumerable<T>)instance;
+        }
+    }
+}
