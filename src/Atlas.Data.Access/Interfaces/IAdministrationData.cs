131d47099195e6974eb12ef94cf2f84d08900cbe
grantcolley <grantcolley@yahoo.com>
2023-11-03 07:49:46 +0000

    Update IAdministrationData.cs

--- src/Atlas.Data.Access/Interfaces/IAdministrationData.cs
+++ src/Atlas.Data.Access/Interfaces/IAdministrationData.cs
@@ -4,20 +4,20 @@ namespace Atlas.Data.Access.Interfaces
 {
     public interface IAdministrationData : IAuthorisationData
     {
-        Task<IEnumerable<User>> GetUsersAsync();
-        Task<User> GetUserAsync(int userId);
-        Task<User> AddUserAsync(User user);
-        Task<User> UpdateUserAsync(User user);
-        Task<int> DeleteUserAsync(int userId);
-        Task<IEnumerable<Permission>> GetPermissionsAsync();
-        Task<Permission> GetPermissionAsync(int permissionId);
-        Task<Permission> AddPermissionAsync(Permission permission);
-        Task<Permission> UpdatePermissionAsync(Permission permission);
-        Task<int> DeletePermissionAsync(int permissionId);
-        Task<IEnumerable<Role>> GetRolesAsync();
-        Task<Role> GetRoleAsync(int roleId);
-        Task<Role> AddRoleAsync(Role role);
-        Task<Role> UpdateRoleAsync(Role role);
-        Task<int> DeleteRoleAsync(int roleId);
+        Task<IEnumerable<User>> GetUsersAsync(CancellationToken cancellationToken);
+        Task<User> GetUserAsync(int userId, CancellationToken cancellationToken);
+        Task<User> AddUserAsync(User user, CancellationToken cancellationToken);
+        Task<User> UpdateUserAsync(User user, CancellationToken cancellationToken);
+        Task<int> DeleteUserAsync(int userId, CancellationToken cancellationToken);
+        Task<IEnumerable<Permission>> GetPermissionsAsync(CancellationToken cancellationToken);
+        Task<Permission> GetPermissionAsync(int permissionId, CancellationToken cancellationToken);
+        Task<Permission> AddPermissionAsync(Permission permission, CancellationToken cancellationToken);
+        Task<Permission> UpdatePermissionAsync(Permission permission, CancellationToken cancellationToken);
+        Task<int> DeletePermissionAsync(int permissionId, CancellationToken cancellationToken);
+        Task<IEnumerable<Role>> GetRolesAsync(CancellationToken cancellationToken);
+        Task<Role> GetRoleAsync(int roleId, CancellationToken cancellationToken);
+        Task<Role> AddRoleAsync(Role role, CancellationToken cancellationToken);
+        Task<Role> UpdateRoleAsync(Role role, CancellationToken cancellationToken);
+        Task<int> DeleteRoleAsync(int roleId, CancellationToken cancellationToken);
     }
 }
