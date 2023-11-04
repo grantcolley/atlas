11191ee0089356e4a126af2e41b5fe6774948619
grantcolley <grantcolley@yahoo.com>
2023-11-04 21:19:38 +0000

    Update OptionsRequest.cs

--- src/Atlas.Requests/API/OptionsRequest.cs
+++ src/Atlas.Requests/API/OptionsRequest.cs
@@ -1,26 +1,45 @@
 ﻿using Atlas.Core.Authentication;
 using Atlas.Core.Constants;
+using Atlas.Core.Extensions;
+using Atlas.Core.Interfaces;
 using Atlas.Core.Models;
+using Atlas.Core.Options;
 using Atlas.Requests.Base;
 using Atlas.Requests.Interfaces;
+using Atlas.Requests.Model;
 using System.Net.Http.Json;
 
 namespace Atlas.Requests.API
 {
     public class OptionsRequest : RequestBase, IOptionsRequest
     {
+        private readonly Dictionary<string, IOptionItems> _localOptionItems = new();
+
         public OptionsRequest(HttpClient httpClient)
-            : base(httpClient)
+            : this(httpClient, null)
         {
         }
 
-        public OptionsRequest(HttpClient httpClient, TokenProvider tokenProvider)
+        public OptionsRequest(HttpClient httpClient, TokenProvider? tokenProvider)
             : base(httpClient, tokenProvider)
         {
+            _localOptionItems.Add(typeof(PageCodeOptionItems).Name, new PageCodeOptionItems());
         }
 
         public async Task<IResponse<IEnumerable<OptionItem>?>> GetOptionItems(IEnumerable<OptionsArg> optionsArgs)
         {
+            var optionsCode = optionsArgs.FirstOptionsArgValue(Options.OPTIONS_CODE);
+
+            if (!string.IsNullOrWhiteSpace(optionsCode)
+                && _localOptionItems.ContainsKey(optionsCode))
+            {
+                return new Response<IEnumerable<OptionItem>?>
+                {
+                    IsSuccess = true,
+                    Result = await _localOptionItems[optionsCode].GetOptionItemsAsync(optionsArgs).ConfigureAwait(false),
+                };
+            }
+
             using var httpResponseMessage = await _httpClient.PostAsJsonAsync(AtlasAPIEndpoints.GET_OPTIONS, optionsArgs)
                 .ConfigureAwait(false);
 
