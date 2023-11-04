711255b048ce405ee69a295925bb49240f8f413f
grantcolley <grantcolley@yahoo.com>
2023-11-04 21:20:07 +0000

    Create PageCodes.cs

---
+++ src/Atlas.Core/Constants/PageCodes.cs
@@ -0,0 +1,18 @@
+﻿namespace Atlas.Core.Constants
+{
+    public static class PageCodes
+    {
+        public const string MODULE = "Module";
+        public const string MODULES = "Modules";
+        public const string CATEGORY = "Category";
+        public const string CATEGORIES = "Categories";
+        public const string MENU_ITEM = "MenuItem";
+        public const string MENU_ITEMS = "MenuItems";
+        public const string USER = "User";
+        public const string USERS = "Users";
+        public const string ROLE = "Role";
+        public const string ROLES = "Roles";
+        public const string PERMISSION = "Permission";
+        public const string PERMISSIONS = "Permissions";
+    }
+}
