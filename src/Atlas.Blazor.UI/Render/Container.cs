ba260db968ef0908f6809c8a8c8e6a24f3dd03bf
grantcolley <grantcolley@yahoo.com>
2023-11-11 22:49:29 +0000

    Create Container.cs

---
+++ src/Atlas.Blazor.UI/Render/Container.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlas.Blazor.UI.Render
+{
+    public class Container
+    {
+        public string? ContainerCode { get; set; }
+        public string? Title { get; set; }
+        public int Order { get; set; }
+    }
+}
