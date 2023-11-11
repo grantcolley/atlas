b489202b3218f0e199beb664658cdb2c168bf0c8
grantcolley <grantcolley@yahoo.com>
2023-11-11 22:49:11 +0000

    Update PropertyRender.cs

--- src/Atlas.Blazor.UI/Render/PropertyRender.cs
+++ src/Atlas.Blazor.UI/Render/PropertyRender.cs
@@ -4,15 +4,18 @@ namespace Atlas.Blazor.UI.Render
 {
     public class PropertyRender<T> : IPropertyRender<T> where T : class, new()
     {
-        public PropertyRender(string propertyName)
+        public PropertyRender(string propertyName, ModelRender<T> modelRender)
         {
             PropertyName = propertyName ?? throw new ArgumentNullException(nameof(propertyName));
+            ModelRender = modelRender ?? throw new ArgumentNullException(nameof(modelRender));
         }
 
         public string PropertyName { get; }
+        public ModelRender<T> ModelRender { get; }
         public int Order { get; set; }
         public string? Label { get; set; }
         public string? Tooltip { get; set; }
+        public string? ContainerCode { get; set; }
         public Dictionary<string, string> Parameters { get; set; } = new();
         public Type? ComponentType { get; set; }
         public Type? GenericType { get; set; }
