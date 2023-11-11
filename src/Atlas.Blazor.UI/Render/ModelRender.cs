3170c1dc6c155d481e677fccc1d0fcfb2288f1b8
grantcolley <grantcolley@yahoo.com>
2023-11-11 22:49:21 +0000

    Update ModelRender.cs

--- src/Atlas.Blazor.UI/Render/ModelRender.cs
+++ src/Atlas.Blazor.UI/Render/ModelRender.cs
@@ -7,6 +7,7 @@ namespace Atlas.Blazor.UI.Render
     public abstract class ModelRender<T> where T : class, new()
     {
         private readonly List<PropertyRender<T>> _propertyRenders = new();
+        private readonly List<Container> _renderContainers = new();
 
         public abstract void Configure();
 
@@ -16,7 +17,7 @@ namespace Atlas.Blazor.UI.Render
 
             if (memberInfo == null) throw new NullReferenceException(nameof(memberInfo));
 
-            PropertyRender<T> propertyRender = new(memberInfo.Name);
+            PropertyRender<T> propertyRender = new(memberInfo.Name, this);
 
             _propertyRenders.Add(propertyRender);
 
@@ -29,13 +30,22 @@ namespace Atlas.Blazor.UI.Render
 
             if (memberInfo == null) throw new NullReferenceException(nameof(memberInfo));
 
-            PropertyRender<T> propertyRender = new(memberInfo.Name);
+            PropertyRender<T> propertyRender = new(memberInfo.Name, this);
 
             propertyRender.GenericType = expression.ReturnType;
 
             _propertyRenders.Add(propertyRender);
 
             return new PropertyRenderBuilder<T>(propertyRender);
+        }
+
+        public void RenderContainer(Container renderContainers)
+        {
+            if (renderContainers == null) throw new ArgumentNullException(nameof(renderContainers));
+            if (renderContainers.ContainerCode == null) throw new NullReferenceException(nameof(renderContainers.ContainerCode));
+            if (renderContainers.Title == null) throw new NullReferenceException(nameof(renderContainers.Title));
+
+            _renderContainers.Add(renderContainers);
         }
 
         public IEnumerable<ModelPropertyRender<T>> GetModelPropertyRenders(T model)
@@ -48,6 +58,17 @@ namespace Atlas.Blazor.UI.Render
             }
 
             return modelPropertyRenders;
+        }
+
+        public IEnumerable<Container> GetContainers()
+        {
+            return _renderContainers.OrderBy(c => c.Order);
+        }
+
+        public bool ContainerExists(string containerCode)
+        {
+            return _renderContainers.Exists(c => !string.IsNullOrWhiteSpace(c.ContainerCode)
+                                                && c.ContainerCode.Equals(containerCode));
         }
     }
 }
