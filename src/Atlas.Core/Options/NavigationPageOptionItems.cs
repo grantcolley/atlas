ae22557ced87f430bf570c0fca29ef13af2a9e41
grantcolley <grantcolley@yahoo.com>
2023-11-14 08:25:14 +0000

    Create NavigationPageOptionItems.cs

---
+++ src/Atlas.Core/Options/NavigationPageOptionItems.cs
@@ -0,0 +1,30 @@
+﻿using Atlas.Core.Attributes;
+using Atlas.Core.Dynamic;
+using Atlas.Core.Interfaces;
+using Atlas.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atlas.Core.Options
+{
+    public class NavigationPageOptionItems : IOptionItems
+    {
+        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync(IEnumerable<OptionsArg> args)
+        {
+            var containers = TypeAttributeHelper.GetHeadwayTypesByAttribute(typeof(NavigationPageAttribute));
+
+            List<OptionItem> optionItems = new();
+
+            optionItems.AddRange((from c in containers
+                                  orderby c.Name
+                                  select new OptionItem
+                                  {
+                                      Id = c.Namespace,
+                                      Display = c.DisplayName
+                                  }).ToList());
+
+            return Task.FromResult(optionItems.AsEnumerable());
+        }
+    }
+}
