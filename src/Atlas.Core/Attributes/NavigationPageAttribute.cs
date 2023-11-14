00cd5be059b32cfd163864c2e8ebfac35cc4390f
grantcolley <grantcolley@yahoo.com>
2023-11-14 08:24:48 +0000

    Create NavigationPageAttribute.cs

---
+++ src/Atlas.Core/Attributes/NavigationPageAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Atlas.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class NavigationPageAttribute : Attribute
+    {
+    }
+}
