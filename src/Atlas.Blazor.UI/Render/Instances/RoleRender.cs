0f14ded26f8a8403b1a6ded07439090bbc960b6a
grantcolley <grantcolley@yahoo.com>
2023-11-14 08:25:23 +0000

    Create RoleRender.cs

---
+++ src/Atlas.Blazor.UI/Render/Instances/RoleRender.cs
@@ -0,0 +1,40 @@
+﻿using Atlas.Blazor.UI.Constants;
+using Atlas.Core.Models;
+
+namespace Atlas.Blazor.UI.Render.Instances
+{
+    public class RoleRender : ModelRender<Role>
+    {
+        public override void Configure()
+        {
+            string roleContainerCode = "ROLE";
+            string permissionsContainerCode = "PERMISSIONS";
+            string usersContainerCode = "USERS";
+
+            RenderContainer(new Container { ContainerCode = roleContainerCode, Title = "Role", Order = 1 });
+            RenderContainer(new Container { ContainerCode = permissionsContainerCode, Title = "Permissions", Order = 2 });
+            RenderContainer(new Container { ContainerCode = usersContainerCode, Title = "Users", Order = 3 });
+
+            RenderProperty(r => r.RoleId)
+                .RenderAs(Element.Label)
+                .InContainer(roleContainerCode)
+                .WithLabel("Role Id")
+                .WithTooltip("Role Id")
+                .RenderOrder(1);
+
+            RenderProperty(r => r.Name)
+                .RenderAs(Element.Text)
+                .InContainer(roleContainerCode)
+                .WithLabel("Name")
+                .WithTooltip("Name")
+                .RenderOrder(2);
+
+            RenderProperty(r => r.Description)
+                .RenderAs(Element.Text)
+                .InContainer(roleContainerCode)
+                .WithLabel("Description")
+                .WithTooltip("Description")
+                .RenderOrder(3);
+        }
+    }
+}
