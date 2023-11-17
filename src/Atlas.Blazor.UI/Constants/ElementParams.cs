4a63e457ce3c47e30deb1440c4311115ece3da45
grantcolley <grantcolley@yahoo.com>
2023-11-17 22:25:50 +0000

    Update ElementParams.cs

--- src/Atlas.Blazor.UI/Constants/ElementParams.cs
+++ src/Atlas.Blazor.UI/Constants/ElementParams.cs
@@ -5,6 +5,8 @@
         public const string GENERIC_MODEL_DISPLAY_FIELD = "GenericDisplayField";
         public const string GENERIC_COMPONENT_NAME = "GenericComponentName";
         public const string IS_NUMERIC_ID = "IsNumericId";
+        public const string LABEL_TYPO = "LabelTypo";
+        public const string LABEL_TYPO_BODY1 = "body1";
         public const string MAX = "Max";
         public const string MAX_LENGTH = "MaxLength";
         public const string MIN = "Min";
