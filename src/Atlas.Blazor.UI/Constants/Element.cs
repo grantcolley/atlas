879554be83100901c95f49aa023a9d634807867d
grantcolley <grantcolley@yahoo.com>
2023-11-19 21:35:57 +0000

    Update Element.cs

--- src/Atlas.Blazor.UI/Constants/Element.cs
+++ src/Atlas.Blazor.UI/Constants/Element.cs
@@ -8,6 +8,7 @@
         public const string DropdownIcon = "Atlas.Blazor.UI.Components.Mud.DropdownIcon`1, Atlas.Blazor.UI";
         public const string Integer = "Atlas.Blazor.UI.Components.Mud.Integer`1, Atlas.Blazor.UI";
         public const string Label = "Atlas.Blazor.UI.Components.Mud.Label`1, Atlas.Blazor.UI";
+        public const string ReadList = "Atlas.Blazor.UI.Components.Custom.ReadList`1, Atlas.Blazor.UI";
         public const string Text = "Atlas.Blazor.UI.Components.Mud.Text`1, Atlas.Blazor.UI";
     }
 }
