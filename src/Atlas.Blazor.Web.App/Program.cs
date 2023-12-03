a2108859d893b9725ffd7ac18db9e156fce9c7ac
grantcolley <grantcolley@yahoo.com>
2023-12-03 10:54:34 +0000

    Update Program.cs

--- src/Atlas.Blazor.Web.App/Program.cs
+++ src/Atlas.Blazor.Web.App/Program.cs
@@ -1,10 +1,83 @@
+using Atlas.Blazor.Helpers;
+using Atlas.Blazor.Interfaces;
+using Atlas.Blazor.Services;
 using Atlas.Blazor.Web.App.Components;
+using Atlas.Core.Authentication;
+using Atlas.Core.Constants;
+using Atlas.Requests.API;
+using Atlas.Requests.Interfaces;
+using Auth0.AspNetCore.Authentication;
+using MudBlazor.Services;
+using System.IdentityModel.Tokens.Jwt;
+using Weather.Client.Requests;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services
+    .AddAuth0WebAppAuthentication(Auth0Constants.AuthenticationScheme, options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Domain"] ?? throw new NullReferenceException("Auth0:Domain");
+        options.ClientId = builder.Configuration["Auth0:ClientId"] ?? throw new NullReferenceException("Auth0:ClientId");
+        options.ClientSecret = builder.Configuration["Auth0:ClientSecret"] ?? throw new NullReferenceException("Auth0:ClientSecret");
+        options.ResponseType = "code";
+    }).WithAccessToken(options =>
+    {
+        options.Audience = builder.Configuration["Auth0:Audience"] ?? throw new NullReferenceException("Auth0:Audience");
+    });
+
+builder.Services.AddHttpClient(AtlasConstants.ATLAS_API, client =>
+{
+    client.BaseAddress = new Uri("https://localhost:44420");
+});
+
+builder.Services.AddSingleton<IPageRouterService, PageRouterService>(sp =>
+{
+    return new PageRouterService(PageRouterHelper.GetPageArgs());
+});
+
+builder.Services.AddScoped<TokenProvider>();
+builder.Services.AddScoped<IStateNotificationService, StateNotificationService>();
+builder.Services.AddTransient<IDialogService, DialogService>();
+
+builder.Services.AddTransient<IUserRequests, UserRequests>(sp =>
+{
+    var tokenProvider = sp.GetRequiredService<TokenProvider>();
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient(AtlasConstants.ATLAS_API);
+    return new UserRequests(httpClient, tokenProvider);
+});
+
+builder.Services.AddTransient<IOptionsRequest, OptionsRequest>(sp =>
+{
+    var tokenProvider = sp.GetRequiredService<TokenProvider>();
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient(AtlasConstants.ATLAS_API);
+    return new OptionsRequest(httpClient, tokenProvider);
+});
+
+builder.Services.AddTransient<IGenericRequests, GenericRequests>(sp =>
+{
+    var tokenProvider = sp.GetRequiredService<TokenProvider>();
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient(AtlasConstants.ATLAS_API);
+    return new GenericRequests(httpClient, tokenProvider);
+});
+
+builder.Services.AddTransient<IWeatherForecastRequests, WeatherForecastRequests>(sp =>
+{
+    var tokenProvider = sp.GetRequiredService<TokenProvider>();
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient(AtlasConstants.ATLAS_API);
+    return new WeatherForecastRequests(httpClient, tokenProvider);
+});
 
 var app = builder.Build();
 
@@ -23,5 +96,11 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapBlazorHub();
 
 app.Run();
