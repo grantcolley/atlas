fe811210f9d222d433a086f9230ddfd31ea2407b
grantcolley <grantcolley@yahoo.com>
2023-12-20 21:52:58 +0000

    Create PersistingRevalidatingAuthenticationStateProvider.cs

---
+++ src/Atlas.Blazor.Web.App/Account/PersistingRevalidatingAuthenticationStateProvider.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Microsoft.AspNetCore.Components.Web;
+using Atlas.Core.Models;
+using Atlas.Requests.Interfaces;
+using System.Linq;
+
+namespace Atlas.Blazor.Web.App.Account
+{
+    // This is a server-side AuthenticationStateProvider that revalidates the security stamp for the connected user
+    // every 30 minutes an interactive circuit is connected. It also uses PersistentComponentState to flow the
+    // authentication state to the client which is then fixed for the lifetime of the WebAssembly application.
+    internal sealed class PersistingRevalidatingAuthenticationStateProvider : RevalidatingServerAuthenticationStateProvider
+    {
+        private readonly IServiceScopeFactory scopeFactory;
+        private readonly PersistentComponentState state;
+        private readonly IdentityOptions options;
+
+        private readonly PersistingComponentStateSubscription subscription;
+
+        private Task<AuthenticationState>? authenticationStateTask;
+
+        public PersistingRevalidatingAuthenticationStateProvider(
+            ILoggerFactory loggerFactory,
+            IServiceScopeFactory serviceScopeFactory,
+            PersistentComponentState persistentComponentState,
+            IOptions<IdentityOptions> optionsAccessor)
+            : base(loggerFactory)
+        {
+            scopeFactory = serviceScopeFactory;
+            state = persistentComponentState;
+            options = optionsAccessor.Value;
+
+            AuthenticationStateChanged += OnAuthenticationStateChanged;
+            subscription = state.RegisterOnPersisting(OnPersistingAsync, RenderMode.InteractiveWebAssembly);
+        }
+
+        protected override TimeSpan RevalidationInterval => TimeSpan.FromMinutes(30);
+
+        protected override async Task<bool> ValidateAuthenticationStateAsync(
+            AuthenticationState authenticationState, CancellationToken cancellationToken)
+        {
+            // Get the user manager from a new scope to ensure it fetches fresh data
+            await using var scope = scopeFactory.CreateAsyncScope();
+            var userRequests = scope.ServiceProvider.GetRequiredService<IUserRequests>();
+            return await ValidateSecurityStampAsync(userRequests, authenticationState.User);
+        }
+
+        private async Task<bool> ValidateSecurityStampAsync(IUserRequests userRequests, ClaimsPrincipal principal)
+        {
+            var authorisation = await userRequests.GetAuthorisationAsync(principal);
+            if (authorisation != null
+                && authorisation.Permissions.Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void OnAuthenticationStateChanged(Task<AuthenticationState> task)
+        {
+            authenticationStateTask = task;
+        }
+
+        private async Task OnPersistingAsync()
+        {
+            if (authenticationStateTask is null)
+            {
+                throw new UnreachableException($"Authentication state not set in {nameof(OnPersistingAsync)}().");
+            }
+
+            var authenticationState = await authenticationStateTask;
+            var principal = authenticationState.User;
+
+            if (principal.Identity?.IsAuthenticated == true)
+            {
+                var userId = principal.FindFirst(options.ClaimsIdentity.UserIdClaimType)?.Value;
+                var email = principal.FindFirst(options.ClaimsIdentity.EmailClaimType)?.Value;
+
+                if (userId != null && email != null)
+                {
+                    state.PersistAsJson(nameof(UserInfo), new UserInfo
+                    {
+                        UserId = userId,
+                        Email = email,
+                    });
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            subscription.Dispose();
+            AuthenticationStateChanged -= OnAuthenticationStateChanged;
+            base.Dispose(disposing);
+        }
+    }
+}
