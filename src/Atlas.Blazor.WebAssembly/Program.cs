892f4fdec3aa294adb84974a287831ffded3294b
grantcolley <grantcolley@yahoo.com>
2023-12-20 21:52:07 +0000

    Update Program.cs

--- src/Atlas.Blazor.WebAssembly/Program.cs
+++ src/Atlas.Blazor.WebAssembly/Program.cs
@@ -1,5 +1,26 @@
+using Atlas.Core.Constants;
+using Atlas.Requests.API;
+using Atlas.Requests.Interfaces;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FluentUI.AspNetCore.Components.Components.Tooltip;
+using System;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddHttpClient(AtlasConstants.ATLAS_API, client =>
+{
+    client.BaseAddress = new Uri("https://localhost:44420");
+});
+
+builder.Services.AddScoped<ITooltipService, TooltipService>();
+
+builder.Services.AddTransient<IUserRequests, UserRequests>(sp =>
+{
+    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = httpClientFactory.CreateClient(AtlasConstants.ATLAS_API);
+    return new UserRequests(httpClient);
+});
 
 await builder.Build().RunAsync();
