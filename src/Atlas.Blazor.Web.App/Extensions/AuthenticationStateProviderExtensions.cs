657dfca46303b8d155fb07e4b423c681245fba32
grantcolley <grantcolley@yahoo.com>
2023-12-20 21:52:55 +0000

    Create AuthenticationStateProviderExtensions.cs

---
+++ src/Atlas.Blazor.Web.App/Extensions/AuthenticationStateProviderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace Atlas.Blazor.Web.App.Extensions
+{
+    public static class AuthenticationStateProviderExtensions
+    {
+        public static async Task<bool> IsAuthenticatedAtlasUser(this AuthenticationStateProvider authenticationStateProvider)
+        {
+            if (authenticationStateProvider == null)
+            {
+                return false;
+            }
+
+            var authState = await authenticationStateProvider.GetAuthenticationStateAsync().ConfigureAwait(false);
+
+            if (authState == null
+                || authState.User == null
+                || authState.User.Identity == null
+                || !authState.User.Identity.IsAuthenticated)
+            {
+                return false;
+            }
+
+            return authState.User.HasClaim(c => c.Value.Equals(Core.Constants.Auth.ATLAS_USER_CLAIM));
+        }
+    }
+}
