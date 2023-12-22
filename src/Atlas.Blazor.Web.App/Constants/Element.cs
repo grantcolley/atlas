d608ad23dcb2eb008878a6ae9ed41fb47a29f696
grantcolley <grantcolley@yahoo.com>
2023-12-22 21:41:16 +0000

    Adding Constants to Atlas.Blazor.Web.App

---
+++ src/Atlas.Blazor.Web.App/Constants/Element.cs
@@ -0,0 +1,14 @@
+﻿namespace Atlas.Blazor.Web.App.Constants
+{
+    public static class Element
+    {
+        public const string Checklist = "Atlas.Blazor.Components.Custom.Checklist`1, Atlas.Blazor";
+        public const string Dropdown = "Atlas.Blazor.Components.Mud.Dropdown`1, Atlas.Blazor";
+        public const string DropdownGeneric = "Atlas.Blazor.Components.Mud.DropdownGeneric`2, Atlas.Blazor";
+        public const string DropdownIcon = "Atlas.Blazor.Components.Mud.DropdownIcon`1, Atlas.Blazor";
+        public const string Integer = "Atlas.Blazor.Components.Mud.Integer`1, Atlas.Blazor";
+        public const string Label = "Atlas.Blazor.Components.Mud.Label`1, Atlas.Blazor";
+        public const string ReadList = "Atlas.Blazor.Components.Custom.ReadList`1, Atlas.Blazor";
+        public const string Text = "Atlas.Blazor.Components.Mud.Text`1, Atlas.Blazor";
+    }
+}
---
+++ src/Atlas.Blazor.Web.App/Constants/StateNotifications.cs
@@ -0,0 +1,7 @@
+﻿namespace Atlas.Blazor.Web.App.Constants
+{
+    public static class StateNotifications
+    {
+        public const string BREADCRUMBS = "Breadcrumbs";
+    }
+}
