36fe8afea57a8140e4fa989ba70cbef41dc8d8e6
grantcolley <grantcolley@yahoo.com>
2023-12-22 21:42:20 +0000

    Create AtlasComponentBase.cs

---
+++ src/Atlas.Blazor.Web.App/Base/AtlasComponentBase.cs
@@ -0,0 +1,89 @@
+﻿using Atlas.Blazor.Web.App.Constants;
+using Atlas.Blazor.Web.App.Interfaces;
+using Atlas.Blazor.Web.App.Models;
+using Atlas.Requests.Interfaces;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace Atlas.Blazor.Web.App.Base
+{
+    public abstract class AtlasComponentBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
+
+        [Inject]
+        public IStateNotificationService? StateNotificationService { get; set; }
+
+        //[Inject]
+        //public IDialogService? DialogService { get; set; }
+
+        protected T? GetResponse<T>(IResponse<T> response)
+        {
+            if (!response.IsSuccess
+                && !string.IsNullOrWhiteSpace(response.Message))
+            {
+                RaiseAlert(response.Message);
+                return default;
+            }
+            else
+            {
+                return response.Result;
+            }
+        }
+
+        protected void RaiseAlert(string message)
+        {
+            var alert = new Models.Alert
+            {
+                AlertType = Alerts.ERROR,
+                Title = "Error",
+                Message = message
+            };
+
+            NavigationManager?.NavigateTo(alert.Page);
+        }
+
+        protected void RaiseAuthorisationAlert(string message)
+        {
+            var alert = new Models.Alert
+            {
+                AlertType = Alerts.ERROR,
+                Title = "Access Denied",
+                Message = message
+            };
+
+            NavigationManager?.NavigateTo(alert.Page);
+        }
+
+        protected async Task SendBreadcrumbAsync(BreadcrumbAction breadcrumbAction, string? text = null, string? href = null)
+        {
+            if (breadcrumbAction == BreadcrumbAction.Add
+                && breadcrumbAction == BreadcrumbAction.Update
+                && (string.IsNullOrWhiteSpace(text)
+                || (string.IsNullOrWhiteSpace(href))))
+            {
+                return;
+            }
+
+            if (StateNotificationService == null) throw new NullReferenceException(nameof(StateNotificationService));
+            if (NavigationManager == null) throw new NullReferenceException(nameof(NavigationManager));
+
+            if (breadcrumbAction == BreadcrumbAction.Add)
+            {
+                href = NavigationManager.Uri.Remove(0, NavigationManager.BaseUri.Length - 1);
+            }
+
+            var breadcrumb = new Breadcrumb
+            {
+                Text = text,
+                Href = href,
+                BreadcrumbAction = breadcrumbAction
+            };
+
+            await StateNotificationService.NotifyStateHasChangedAsync(StateNotifications.BREADCRUMBS, breadcrumb)
+                .ConfigureAwait(false);
+        }
+    }
+}
