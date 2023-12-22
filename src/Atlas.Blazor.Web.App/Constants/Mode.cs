a9bb67fe816428f2dea2d5bb6fa2c3dfb71eb43f
grantcolley <grantcolley@yahoo.com>
2023-12-22 21:42:04 +0000

    Create Mode.cs

---
+++ src/Atlas.Blazor.Web.App/Constants/Mode.cs
@@ -0,0 +1,8 @@
+﻿namespace Atlas.Blazor.Web.App.Constants
+{
+    public static class Mode
+    {
+        public const string DARK = "Change to dark mode";
+        public const string LIGHT = "Change to light mode";
+    }
+}
