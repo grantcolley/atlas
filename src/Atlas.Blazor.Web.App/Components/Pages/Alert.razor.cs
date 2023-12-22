4b10b3e87875f4986802e84f7a5b6b1ad60bbbae
grantcolley <grantcolley@yahoo.com>
2023-12-22 21:42:30 +0000

    Adding components to Atlas.Blazor.Web.App

---
+++ src/Atlas.Blazor.Web.App/Components/Pages/Alert.razor.cs
@@ -0,0 +1,34 @@
+﻿using Atlas.Blazor.Web.App.Constants;
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Atlas.Blazor.Web.App.Pages
+{
+    public abstract class AlertBase : ComponentBase
+    {
+        [Parameter]
+        public string? AlertType { get; set; }
+
+        [Parameter]
+        public string? Title { get; set; }
+
+        [Parameter]
+        public string? Message { get; set; }
+
+        protected MessageIntent _intent;
+
+        protected override void OnInitialized()
+        {
+            _intent = AlertType switch
+            {
+                Alerts.INFO => MessageIntent.Info,
+                Alerts.SUCCESS => MessageIntent.Success,
+                Alerts.WARNING => MessageIntent.Warning,
+                Alerts.ERROR => MessageIntent.Error,
+                _ => MessageIntent.Info,
+            };
+
+            base.OnInitialized();
+        }
+    }
+}
