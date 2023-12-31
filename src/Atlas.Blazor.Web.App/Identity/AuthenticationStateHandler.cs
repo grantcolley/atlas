3274e7f73a809a1f6becf7e3d57c466de445db4d
grantcolley <grantcolley@yahoo.com>
2023-12-31 19:35:17 +0000

    Create AuthenticationStateHandler.cs

---
+++ src/Atlas.Blazor.Web.App/Identity/AuthenticationStateHandler.cs
@@ -0,0 +1,41 @@
+﻿using Atlas.Core.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Atlas.Blazor.Web.App.Identity
+{
+    public class AuthenticationStateHandler : DelegatingHandler
+    {
+        readonly CircuitServicesAccessor? circuitServicesAccessor;
+
+        public AuthenticationStateHandler(
+            CircuitServicesAccessor circuitServicesAccessor)
+        {
+            this.circuitServicesAccessor = circuitServicesAccessor;
+
+            TokenProvider? tokenProvider = circuitServicesAccessor?.Services?
+                .GetRequiredService<TokenProvider>();
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            TokenProvider? tokenProvider = circuitServicesAccessor?.Services?
+                .GetRequiredService<TokenProvider>();
+
+            if(tokenProvider != null) 
+            {
+                if (tokenProvider != null
+                    && !string.IsNullOrWhiteSpace(tokenProvider.AccessToken)
+                    && request.Headers.Authorization == null)
+                {
+                    request.Headers.Add("Authorization", $"Bearer {tokenProvider.AccessToken}");
+                }
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
