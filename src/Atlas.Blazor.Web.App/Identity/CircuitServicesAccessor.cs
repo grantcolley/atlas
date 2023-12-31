6fd4ad9262c64bc7b6b0083260103e321fbe9435
grantcolley <grantcolley@yahoo.com>
2023-12-31 19:35:25 +0000

    Create CircuitServicesAccessor.cs

---
+++ src/Atlas.Blazor.Web.App/Identity/CircuitServicesAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Atlas.Blazor.Web.App.Identity
+{
+    public class CircuitServicesAccessor
+    {
+        static readonly AsyncLocal<IServiceProvider?> blazorServices = new();
+
+        public IServiceProvider? Services
+        {
+            get => blazorServices.Value;
+            set => blazorServices.Value = value;
+        }
+    }
+}
