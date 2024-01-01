1721eca4ba6bbdbce51dfdf1aba846870f650573
grantcolley <grantcolley@yahoo.com>
2024-01-01 21:01:16 +0000

    Update AtlasComponentBase.cs

--- src/Atlas.Blazor.Web.App/Base/AtlasComponentBase.cs
+++ src/Atlas.Blazor.Web.App/Base/AtlasComponentBase.cs
@@ -1,23 +1,69 @@
 ﻿using Atlas.Blazor.Web.App.Constants;
 using Atlas.Blazor.Web.App.Interfaces;
 using Atlas.Blazor.Web.App.Models;
+using Atlas.Core.Authentication;
 using Atlas.Requests.Interfaces;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using System;
 using System.Threading.Tasks;
 
 namespace Atlas.Blazor.Web.App.Base
 {
-    public abstract class AtlasComponentBase : ComponentBase
+    public abstract class AtlasComponentBase : ComponentBase, IDisposable
     {
+        [Inject]
+        public PersistentComponentState? ApplicationState { get; set; }
+
         [Inject]
         public NavigationManager? NavigationManager { get; set; }
 
         [Inject]
         public IStateNotificationService? StateNotificationService { get; set; }
 
+        [Inject]
+        public TokenProvider? SourceTokenProvider { get; set; }
+
         //[Inject]
         //public IDialogService? DialogService { get; set; }
+
+        protected TokenProvider? TokenProvider;
+
+        private PersistingComponentStateSubscription persistingComponentStateSubscription;
+
+        protected override Task OnInitializedAsync()
+        {
+            if (ApplicationState == null) throw new NullReferenceException(nameof(ApplicationState));
+            if (SourceTokenProvider == null) throw new NullReferenceException(nameof(SourceTokenProvider));
+
+            persistingComponentStateSubscription = ApplicationState.RegisterOnPersisting(PersistTokenProvider);
+
+            if (ApplicationState.TryTakeFromJson<TokenProvider>(PersistState.TOKEN_PROVIDER, out var tokenProvider))
+            {
+                TokenProvider = tokenProvider;
+            }
+            else
+            {
+                TokenProvider = SourceTokenProvider;
+            }
+
+            return base.OnInitializedAsync();
+        }
+
+        private Task PersistTokenProvider()
+        {
+            if (ApplicationState == null) throw new NullReferenceException(nameof(ApplicationState));
+            if (TokenProvider == null) throw new NullReferenceException(nameof(TokenProvider));
+
+            ApplicationState.PersistAsJson(PersistState.TOKEN_PROVIDER, SourceTokenProvider);
+
+            return Task.CompletedTask;
+        }
+
+        void IDisposable.Dispose()
+        {
+            persistingComponentStateSubscription.Dispose();
+        }
 
         protected T? GetResponse<T>(IResponse<T> response)
         {
