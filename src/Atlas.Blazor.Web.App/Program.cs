1c3da3bda0c1b9cc86a8149e4735d1281809d945
grantcolley <grantcolley@yahoo.com>
2024-04-01 20:58:31 +0100

    Add Atlas.Blazor.Web.App

---
+++ src/Atlas.Blazor.Web.App/Program.cs
@@ -0,0 +1,28 @@
+using Atlas.Blazor.Web.App.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddAdditionalAssemblies(typeof(Atlas.Blazor.Components._Imports).Assembly)
+    .AddInteractiveServerRenderMode();
+
+app.Run();
