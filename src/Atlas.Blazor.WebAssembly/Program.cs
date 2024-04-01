9609c916c0913d1e1e02c86f3f1541d8353962b8
grantcolley <grantcolley@yahoo.com>
2024-04-01 16:30:47 +0100

    dev in progress

--- src/Atlas.Blazor.WebAssembly/Program.cs
+++ src/Atlas.Blazor.WebAssembly/Program.cs
@@ -1,33 +1,11 @@
-using Atlas.Blazor.WebAssembly.Client.Pages;
-using Atlas.Blazor.WebAssembly.Components;
+using Atlas.Blazor.WebAssembly;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(Atlas.Blazor.WebAssembly.Client._Imports).Assembly);
-
-app.Run();
+await builder.Build().RunAsync();
