558cb7366a0accfb52f907a7a4eed5d4cb43a23f
grantcolley <grantcolley@yahoo.com>
2024-04-03 21:37:19 +0100

    Update Program.cs

--- src/Atlas.Blazor.WebAssembly/Program.cs
+++ src/Atlas.Blazor.WebAssembly/Program.cs
@@ -1,10 +1,19 @@
 using Atlas.Blazor.WebAssembly;
+using Atlas.Blazor.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components.Components.Tooltip;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, AtlasAuthenticationStateProvider>();
+builder.Services.AddScoped<ITooltipService, TooltipService>();
+// https://code-maze.com/authenticationstateprovider-blazor-webassembly/
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
