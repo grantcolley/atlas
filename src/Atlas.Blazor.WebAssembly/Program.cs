5e0edf762c0cf051ddecb96032dc53d6308b939c
grantcolley <grantcolley@yahoo.com>
2024-04-04 21:06:06 +0100

    Update Program.cs

--- src/Atlas.Blazor.WebAssembly/Program.cs
+++ src/Atlas.Blazor.WebAssembly/Program.cs
@@ -3,11 +3,14 @@ using Atlas.Blazor.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components.Components.Tooltip;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
