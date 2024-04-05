b57d00d6c6dbebef1e9570020283799fe09bcf5e
grantcolley <grantcolley@yahoo.com>
2024-04-05 08:26:01 +0100

    Update Program.cs

--- src/Atlas.Blazor.WebAssembly/Program.cs
+++ src/Atlas.Blazor.WebAssembly/Program.cs
@@ -25,26 +25,8 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<AuthenticationStateProvider, AtlasAuthenticationStateProvider>();
 builder.Services.AddScoped<ITooltipService, TooltipService>();
 // https://code-maze.com/authenticationstateprovider-blazor-webassembly/
+// https://learn.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/standalone-with-authentication-library?view=aspnetcore-8.0&tabs=visual-studio
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-app.MapGet("/login", async (HttpContext httpContext, string redirectUri = @"/") =>
-{
-    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
-            .WithRedirectUri(redirectUri)
-            .Build();
-
-    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
-});
-
-app.MapGet("/logout", async (HttpContext httpContext, string redirectUri = @"/") =>
-{
-    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
-            .WithRedirectUri(redirectUri)
-            .Build();
-
-    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
-    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-});
 
 await builder.Build().RunAsync();
