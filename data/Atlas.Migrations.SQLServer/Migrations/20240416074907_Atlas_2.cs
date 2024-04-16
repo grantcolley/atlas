7fc8d98a1586c8b584983ec1014c24114fa6a018
grantcolley <grantcolley@yahoo.com>
2024-04-16 14:18:35 +0100

    Update SQL Server migrations

---
+++ data/Atlas.Migrations.SQLServer/Migrations/20240416074907_Atlas_2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas.Migrations.SQLServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Atlas_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PageCode",
+                table: "MenuItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PageCode",
+                table: "MenuItems",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
