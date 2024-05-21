7e717646e916606c482b3cb4454ce40b82368b11
grantcolley <grantcolley@yahoo.com>
2024-05-21 21:51:42 +0100

    Create AtlasDialogContent.cs

---
+++ src/Atlas.Blazor.Web/Models/AtlasDialogContent.cs
@@ -0,0 +1,16 @@
+﻿namespace Atlas.Blazor.Web.Models
+{
+    public class AtlasDialogContent
+    {
+        public string? Title { get; set; }
+        public bool ShowTitle { get; set; } = true;
+        public List<string> Messages { get; set; } = [];
+        public AtlasDialogType DialogType { get; set; } = AtlasDialogType.Ok;
+        public AtlasDialogSelection Selected { get; set; } = AtlasDialogSelection.None;
+        public bool ShowSecondaryButton { get; set; } = false;
+        public string? Height { get; set; } = "320px";
+        public string? Width { get; set; } = "500px";
+        public bool? Modal { get; set; } = true;
+        public bool PreventScroll { get; set; } = false;
+    }
+}
