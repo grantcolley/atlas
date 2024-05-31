0b061f055fcba03b89c765a9a6f27f01f88f7549
grantcolley <grantcolley@yahoo.com>
2024-05-31 13:47:07 +0100

    Create 20240531124501_Atlas_3.cs

---
+++ data/Atlas.Migrations.SQLServer/Migrations/20240531124501_Atlas_3.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas.Migrations.SQLServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Atlas_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "Name");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                newName: "IX_Users_Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "UserName");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Name",
+                table: "Users",
+                newName: "IX_Users_UserName");
+        }
+    }
+}
