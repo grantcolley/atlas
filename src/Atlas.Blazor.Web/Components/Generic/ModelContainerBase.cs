3fd31aa58e96f1c7b5e8db1cdd69492dc1c53b63
grantcolley <grantcolley@yahoo.com>
2024-06-01 19:14:00 +0100

    Create ModelContainerBase.cs

---
+++ src/Atlas.Blazor.Web/Components/Generic/ModelContainerBase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Blazor.Web.Components.Generic
+{
+    public abstract class ModelContainerBase<T> : GenericModelContainer<T> where T : class, new()
+    {
+        public abstract string? Title { get; set; }
+        public abstract string? APIGetEndpoint { get; set; }
+        public abstract string? APICreateEndpoint { get; set; }
+        public abstract string? APIUpdateEndpoint { get; set; }
+        public abstract string? APIDeleteEndpoint { get; set; }
+        public abstract string? IdentityFieldName { get; set; }
+        public abstract string? ModelNameField { get; set; }
+
+        public abstract RenderFragment RenderModelContent();
+
+        protected override async Task OnInitializedAsync()
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(Title);
+            ArgumentException.ThrowIfNullOrWhiteSpace(APIGetEndpoint);
+            ArgumentException.ThrowIfNullOrWhiteSpace(APICreateEndpoint);
+            ArgumentException.ThrowIfNullOrWhiteSpace(APIUpdateEndpoint);
+            ArgumentException.ThrowIfNullOrWhiteSpace(APIDeleteEndpoint);
+            ArgumentException.ThrowIfNullOrWhiteSpace(IdentityFieldName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(ModelNameField);
+
+            _title = Title;
+            _apiGetEndpoint = APIGetEndpoint;
+            _apiCreateEndpoint = APICreateEndpoint;
+            _apiUpdateEndpoint = APIUpdateEndpoint;
+            _apiDeleteEndpoint = APIDeleteEndpoint;
+            _identityFieldName = IdentityFieldName;
+            _modelNameField = ModelNameField;
+
+            await base.OnInitializedAsync();
+        }
+
+        public override RenderFragment RenderModel()
+        {
+            return RenderModelContent();
+        }
+    }
+}
