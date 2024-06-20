b32e746212b47080620a48d7e45c9dfe110e6e43
Grant Colley <grantcolley@yahoo.com>
2024-06-20 18:28:14 +0100

    Create AtlasComponentBase.cs

---
+++ src/Atlas.Blazor.Web/Components/Base/AtlasComponentBase.cs
@@ -0,0 +1,38 @@
+﻿using Atlas.Blazor.Web.Constants;
+using Atlas.Requests.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Blazor.Web.Components.Base
+{
+    public abstract class AtlasComponentBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
+
+        protected T? GetResponse<T>(IResponse<T> response)
+        {
+            if (!response.IsSuccess
+                && !string.IsNullOrWhiteSpace(response.Message))
+            {
+                NavigateAlertError(response.Message);
+                return default;
+            }
+            else
+            {
+                return response.Result;
+            }
+        }
+
+        protected void NavigateAlertError(string message)
+        {
+            var alert = new Models.Alert
+            {
+                AlertType = Alerts.ERROR,
+                Title = "Error",
+                Message = message
+            };
+
+            NavigationManager?.NavigateTo(alert.Page);
+        }
+    }
+}
