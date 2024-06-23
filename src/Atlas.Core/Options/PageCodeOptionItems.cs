50ce540f8603663cb661ce7d8edc1e521739e4aa
Grant Colley <grantcolley@yahoo.com>
2024-06-23 09:15:36 +0100

    Update PageCodeOptionItems.cs

--- src/Atlas.Core/Options/PageCodeOptionItems.cs
+++ src/Atlas.Core/Options/PageCodeOptionItems.cs
@@ -4,32 +4,23 @@ using Atlas.Core.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Atlas.Core.Options
 {
     public class PageCodeOptionItems : IOptionItems
     {
-        public Task<IEnumerable<OptionItem>> GetOptionItemsAsync(IEnumerable<OptionsArg> args)
+        public IEnumerable<OptionItem> GetOptionItems(IEnumerable<OptionsArg> args)
         {
-            IEnumerable<OptionItem>? options = typeof(PageCodes).GetFields(BindingFlags.Public | BindingFlags.Static)
+            IEnumerable<OptionItem> optionsItems = typeof(PageCodes).GetFields(BindingFlags.Public | BindingFlags.Static)
               .Where(f => f.FieldType == typeof(string))
               .Select(f => new OptionItem
               {
                   Id = (string?)f.GetValue(null),
                   Display = (string?)f.GetValue(null)
               })
-              .OrderBy(oi => oi.Display)
-              .ToList();
+              .OrderBy(oi => oi.Display);
 
-            List<OptionItem> optionItems = new();
-
-            if(options != null)
-            {
-                optionItems.AddRange(options);
-            }
-
-            return Task.FromResult(optionItems.AsEnumerable());
+            return optionsItems;
         }
     }
 }
