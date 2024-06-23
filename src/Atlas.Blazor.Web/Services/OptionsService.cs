c2fa7f39e211aebd8159648248681a045a303c87
Grant Colley <grantcolley@yahoo.com>
2024-06-23 09:16:41 +0100

    Create OptionsService.cs

---
+++ src/Atlas.Blazor.Web/Services/OptionsService.cs
@@ -0,0 +1,43 @@
+﻿using Atlas.Blazor.Web.Interfaces;
+using Atlas.Blazor.Web.OptionItems;
+using Atlas.Core.Constants;
+using Atlas.Core.Extensions;
+using Atlas.Core.Interfaces;
+using Atlas.Core.Models;
+using Atlas.Core.Options;
+
+namespace Atlas.Blazor.Web.Services
+{
+    public class OptionsService : IOptionsService
+    {
+        private readonly Dictionary<string, IOptionItems> _options = [];
+
+        public OptionsService()
+        {
+            _options.Add(Options.PAGE_CODES, new PageCodeOptionItems());
+            _options.Add(Options.NAVIGATION_PAGES, new NavigationPageOptionItems());
+            _options.Add(Options.ICON_SIZE20_OPTION_ITEMS, new IconSize20OptionItems());
+        }
+
+        public IEnumerable<OptionItem> GetOptionItems(string optionsCode)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(optionsCode);
+
+            List<OptionsArg> options = [new() { Name = Options.OPTIONS_CODE, Value = optionsCode }];
+            return GetOptionItems(options);
+        }
+
+        public IEnumerable<OptionItem> GetOptionItems(IEnumerable<OptionsArg> optionsArgs)
+        {
+            ArgumentNullException.ThrowIfNull(optionsArgs);
+
+            string? optionsCode = optionsArgs.FirstOptionsArgValue(Options.OPTIONS_CODE);
+
+            if (optionsCode == null) throw new NullReferenceException(nameof(optionsCode));
+
+            if (!_options.TryGetValue(optionsCode, out IOptionItems? value)) throw new KeyNotFoundException(nameof(optionsCode));
+
+            return value.GetOptionItems(optionsArgs);
+        }
+    }
+}
