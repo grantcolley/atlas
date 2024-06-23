07fd0fca3e89971f6da8d7cea33bdff12efb5715
Grant Colley <grantcolley@yahoo.com>
2024-06-23 09:16:35 +0100

    Create IOptionsService.cs

---
+++ src/Atlas.Blazor.Web/Interfaces/IOptionsService.cs
@@ -0,0 +1,10 @@
+﻿using Atlas.Core.Models;
+
+namespace Atlas.Blazor.Web.Interfaces
+{
+    public interface IOptionsService
+    {
+        IEnumerable<OptionItem> GetOptionItems(string optionsCode);
+        IEnumerable<OptionItem> GetOptionItems(IEnumerable<OptionsArg> optionsArgs);
+    }
+}
