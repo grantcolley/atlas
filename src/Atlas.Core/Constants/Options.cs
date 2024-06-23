aa754d71c0b54650e4a03eafda8f9f2b53e898b1
Grant Colley <grantcolley@yahoo.com>
2024-06-23 09:15:43 +0100

    Update Options.cs

--- src/Atlas.Core/Constants/Options.cs
+++ src/Atlas.Core/Constants/Options.cs
@@ -8,5 +8,6 @@
         public const string CATEGORIES_OPTION_ITEMS = "Categories";
         public const string PAGE_CODES = "PageCodes";
         public const string NAVIGATION_PAGES = "NavigationPages";
+        public const string ICON_SIZE20_OPTION_ITEMS = "IconsSize20";
     }
 }
