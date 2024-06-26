7e82e4b563c94d192eb67c389f6a11ca15957fdf
Grant Colley <grantcolley@yahoo.com>
2024-06-26 23:00:35 +0100

    Update IOptionsService.cs

--- src/Atlas.Blazor.Web/Interfaces/IOptionsService.cs
+++ src/Atlas.Blazor.Web/Interfaces/IOptionsService.cs
@@ -4,7 +4,6 @@ namespace Atlas.Blazor.Web.Interfaces
 {
     public interface IOptionsService
     {
-        bool ContainsOptions(string optionsCode);
         IEnumerable<OptionItem> GetOptionItems(string optionsCode);
         IEnumerable<OptionItem> GetOptionItems(IEnumerable<OptionsArg> optionsArgs);
     }
