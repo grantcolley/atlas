278bc7614dfd17a5c3b7a81f128f053f3639f68b
Grant Colley <grantcolley@yahoo.com>
2024-06-26 22:59:57 +0100

    Delete IconsDialogContent.cs

--- src/Atlas.Blazor.Web/Models/IconsDialogContent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Atlas.Blazor.Web.Models
-{
-    public class IconsDialogContent
-    {
-        public string? Height { get; set; } = "320px";
-        public string? Width { get; set; } = "500px";
-        public string? Icon { get; set; }
-    }
-}
