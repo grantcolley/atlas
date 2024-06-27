ce0e82d895cf7c69da8f43336508f951ac4f919f
Grant Colley <grantcolley@yahoo.com>
2024-06-27 08:11:22 +0100

    Update IOptionsRequest.cs

--- src/Atlas.Requests/Interfaces/IOptionsRequest.cs
+++ src/Atlas.Requests/Interfaces/IOptionsRequest.cs
@@ -4,8 +4,8 @@ namespace Atlas.Requests.Interfaces
 {
     public interface IOptionsRequest
     {
-        Task<IResponse<IEnumerable<OptionItem>?>> GetOptionItems(string optionsCode);
-        Task<IResponse<IEnumerable<OptionItem>?>> GetOptionItems(IEnumerable<OptionsArg> optionsArgs);
+        Task<IResponse<IEnumerable<OptionItem>?>> GetOptionItemsAsync(string optionsCode);
+        Task<IResponse<IEnumerable<OptionItem>?>> GetOptionItemsAsync(IEnumerable<OptionsArg> optionsArgs);
         Task<IResponse<IEnumerable<T>>> GetOptionItemsAsync<T>(IEnumerable<OptionsArg> optionsArgs);
     }
 }
