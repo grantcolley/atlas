54c7c34a4210b59b86368a48df24f71c2ea3b59f
Grant Colley <grantcolley@yahoo.com>
2024-07-12 15:43:59 +0100

    Create AtlasRoutesService.cs

---
+++ src/Atlas.Blazor.Web/Services/AtlasRoutesService.cs
@@ -0,0 +1,24 @@
+﻿using Atlas.Blazor.Web.Interfaces;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleToAttribute("Atlas.Blazor.Web.App")]
+
+namespace Atlas.Blazor.Web.Services
+{
+    public class AtlasRoutesService : IAtlasRoutesService
+    {
+        private readonly List<string> _routes = [];
+
+        internal void AddRoutes(IEnumerable<string> routes)
+        {
+            ArgumentNullException.ThrowIfNull(routes, nameof(routes));
+
+            _routes.AddRange(routes);
+        }
+
+        public IEnumerable<string> GetRoutes()
+        {
+            return _routes.Order();
+        }
+    }
+}
