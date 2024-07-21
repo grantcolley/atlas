7632c02c045d94c7ad2e224f553f90dde617d8b4
Grant Colley <grantcolley@yahoo.com>
2024-07-21 20:05:01 +0100

    Create APIResult.cs #6

---
+++ src/Atlas.Core/Models/APIResult.cs
@@ -0,0 +1,16 @@
+﻿using Atlas.Core.Interfaces;
+
+namespace Atlas.Core.Models
+{
+    public class APIResult<T> : IAPIResult<T> where T : new()
+    {
+        public APIResult(Authorisation authorisation, T result) 
+        {
+            Authorisation = authorisation;
+            Result = result;
+        }
+
+        public Authorisation Authorisation { get; }
+        public T Result { get; }
+    }
+}
