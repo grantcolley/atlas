7490768b841a12ae679272015b9ced14a7c7fd87
Grant Colley <grantcolley@yahoo.com>
2024-07-23 07:33:36 +0100

    Create AuthResult.cs

---
+++ src/Atlas.Requests/Model/AuthResult.cs
@@ -0,0 +1,12 @@
+﻿using Atlas.Core.Models;
+using Atlas.Requests.Interfaces;
+
+namespace Atlas.Requests.Model
+{
+    public class AuthResult<T> : IAuthResult<T> where T : new()
+    {
+        public Authorisation Authorisation { get; set; }
+
+        public T Result => throw new NotImplementedException();
+    }
+}
