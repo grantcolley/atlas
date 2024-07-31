00ae31afdc30a4a76c1c74e3b728d26754d5c988
Grant Colley <grantcolley@yahoo.com>
2024-07-31 15:04:36 +0100

    Create AtlasException.cs

---
+++ src/Atlas.Core/Exceptions/AtlasException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Atlas.Core.Exceptions
+{
+    public class AtlasException : Exception
+    {
+        public AtlasException()
+        {
+        }
+
+        public AtlasException(string message)
+            : base(message)
+        {
+        }
+
+        public AtlasException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
