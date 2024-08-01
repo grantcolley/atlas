aba70d367403c1baf172b197bfe75a915210ddfd
Grant Colley <grantcolley@yahoo.com>
2024-08-01 21:21:29 +0100

    Update ClaimData.cs

--- src/Atlas.Data.Access/Data/ClaimData.cs
+++ src/Atlas.Data.Access/Data/ClaimData.cs
@@ -1,4 +1,5 @@
-﻿using Atlas.Core.Models;
+﻿using Atlas.Core.Exceptions;
+using Atlas.Core.Models;
 using Atlas.Data.Access.Base;
 using Atlas.Data.Access.Context;
 using Atlas.Data.Access.Interfaces;
@@ -12,46 +13,55 @@ namespace Atlas.Data.Access.Data
     {
         public async Task<IEnumerable<Module>?> GetApplicationClaimsAsync(CancellationToken cancellationToken)
         {
-            string? email = _applicationDbContext.GetUser();
+            string? email = null;
 
-            if (string.IsNullOrWhiteSpace(email))
+            try
             {
-                return default;
+                email = _applicationDbContext.GetUser();
+
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    return default;
+                }
+
+                User? user = await _applicationDbContext.Users
+                    .Include(u => u.Roles)
+                    .ThenInclude(r => r.Permissions)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(u => u.Email != null && u.Email.Equals(email), cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (user == null)
+                {
+                    return default;
+                }
+
+                List<string?> rolePermissions = user.Roles
+                    .SelectMany(r => r.Permissions)
+                    .Select(p => p.Code)
+                    .ToList();
+
+                List<string?> permissions = rolePermissions.Distinct().ToList();
+
+                List<Module> modules = await _applicationDbContext.Modules
+                    .AsNoTracking()
+                    .Include(m => m.Categories.OrderBy(c => c.Order))
+                    .ThenInclude(c => c.Pages.OrderBy(mu => mu.Order))
+                    .Where(m => permissions.Contains(m.Permission))
+                    .OrderBy(m => m.Order)
+                    .ToListAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                List<Module> permittedModules = modules
+                    .Where(m => m.IsPermitted(permissions))
+                    .ToList();
+
+                return permittedModules;
             }
-
-            User? user = await _applicationDbContext.Users
-                .Include(u => u.Roles)
-                .ThenInclude(r => r.Permissions)
-                .AsNoTracking()
-                .FirstOrDefaultAsync(u => u.Email != null && u.Email.Equals(email), cancellationToken)
-                .ConfigureAwait(false);
-
-            if (user == null)
+            catch (Exception ex)
             {
-                return default;
+                throw new AtlasException(ex.Message, ex, $"User={email ?? "Email is null"}");
             }
-
-            List<string?> rolePermissions = user.Roles
-                .SelectMany(r => r.Permissions)
-                .Select(p => p.Code)
-                .ToList();
-
-            List<string?> permissions = rolePermissions.Distinct().ToList();
-
-            List<Module> modules = await _applicationDbContext.Modules
-                .AsNoTracking()
-                .Include(m => m.Categories.OrderBy(c => c.Order))
-                .ThenInclude(c => c.Pages.OrderBy(mu => mu.Order))
-                .Where(m => permissions.Contains(m.Permission))
-                .OrderBy(m => m.Order)
-                .ToListAsync(cancellationToken)
-                .ConfigureAwait(false);
-
-            List<Module> permittedModules = modules
-                .Where(m => m.IsPermitted(permissions))
-                .ToList();
-
-            return permittedModules;
         }
     }
 }
