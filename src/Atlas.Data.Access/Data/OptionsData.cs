9e959940fda4d2231e1a04ba1ac67b2b1cd30270
Grant Colley <grantcolley@yahoo.com>
2024-08-01 21:21:32 +0100

    Update OptionsData.cs

--- src/Atlas.Data.Access/Data/OptionsData.cs
+++ src/Atlas.Data.Access/Data/OptionsData.cs
@@ -1,4 +1,5 @@
 ﻿using Atlas.Core.Constants;
+using Atlas.Core.Exceptions;
 using Atlas.Core.Extensions;
 using Atlas.Core.Models;
 using Atlas.Data.Access.Base;
@@ -28,32 +29,44 @@ namespace Atlas.Data.Access.Data
         {
             string? optionsCode = optionsArgs.FirstOptionsArgValue(Options.OPTIONS_CODE);
 
-            if(!string.IsNullOrWhiteSpace(optionsCode))
+            if (string.IsNullOrWhiteSpace(optionsCode)) throw new AtlasException($"{nameof(optionsCode)} is null", new NullReferenceException(nameof(optionsCode)));
+
+            if (optionItems.TryGetValue(optionsCode, out Func<IEnumerable<OptionsArg>, CancellationToken, Task<IEnumerable<OptionItem>>>? value))
             {
-                if (optionItems.TryGetValue(optionsCode, out Func<IEnumerable<OptionsArg>, CancellationToken, Task<IEnumerable<OptionItem>>>? value))
+                try
                 {
                     return await value.Invoke(optionsArgs, cancellationToken)
                         .ConfigureAwait(false);
                 }
+                catch(Exception ex)
+                {
+                    throw new AtlasException(ex.Message, ex, $"Options Code = {optionsCode}");
+                }
             }
 
-            throw new NotImplementedException(optionsCode);
+            throw new AtlasException($"{optionsCode} not found", new NotImplementedException(optionsCode), $"Options Code = {optionsCode}");
         }
 
         public async Task<string> GetGenericOptionsAsync(IEnumerable<OptionsArg> optionsArgs, CancellationToken cancellationToken)
         {
             string? optionsCode = optionsArgs.FirstOptionsArgValue(Options.OPTIONS_CODE);
 
-            if (!string.IsNullOrWhiteSpace(optionsCode))
+            if (string.IsNullOrWhiteSpace(optionsCode)) throw new AtlasException($"{nameof(optionsCode)} is null", new NullReferenceException(nameof(optionsCode)));
+
+            if (genericOptionItems.TryGetValue(optionsCode, out Func<IEnumerable<OptionsArg>, CancellationToken, Task<string>>? value))
             {
-                if (genericOptionItems.TryGetValue(optionsCode, out Func<IEnumerable<OptionsArg>, CancellationToken, Task<string>>? value))
+                try
                 {
                     return await value.Invoke(optionsArgs, cancellationToken)
                         .ConfigureAwait(false);
                 }
+                catch (Exception ex)
+                {
+                    throw new AtlasException(ex.Message, ex, $"Options Code = {optionsCode}");
+                }
             }
 
-            throw new NotImplementedException(optionsCode);
+            throw new AtlasException($"{optionsCode} not found", new NotImplementedException(optionsCode), $"Options Code = {optionsCode}");
         }
 
         private async Task<IEnumerable<OptionItem>> GetPermissionsOptionItemsAsync(IEnumerable<OptionsArg> optionsArgs, CancellationToken cancellationToken)
