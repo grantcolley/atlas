7a96a1594f8215919035c6c15708b77fb219d0e9
Grant Colley <grantcolley@yahoo.com>
2024-08-04 21:58:08 +0100

    Update SupportEndpoints.cs

--- src/Atlas.API/Endpoints/SupportEndpoints.cs
+++ src/Atlas.API/Endpoints/SupportEndpoints.cs
@@ -1,20 +1,28 @@
 ﻿using Atlas.API.Interfaces;
+using Atlas.Blazor.Web.Pages.Developer;
 using Atlas.Core.Constants;
 using Atlas.Core.Exceptions;
 using Atlas.Core.Models;
 using Atlas.Data.Access.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace Atlas.API.Endpoints
 {
     internal static class SupportEndpoints
     {
-        internal static async Task<IResult> GetLogs([FromBody] LogArgs logArgs, ISupportData supportData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
+        internal static async Task<IResult> GetLogs([FromBody] string args, ISupportData supportData, IClaimService claimService, ILogService logService, CancellationToken cancellationToken)
         {
             Authorisation? authorisation = null;
 
             try
             {
+                ArgumentNullException.ThrowIfNullOrWhiteSpace(args, nameof(args));
+
+                LogArgs? logArgs = JsonSerializer.Deserialize<LogArgs>(args);
+
+                ArgumentNullException.ThrowIfNull(logArgs, nameof(logArgs));
+
                 authorisation = await supportData.GetAuthorisationAsync(claimService.GetClaim(), cancellationToken)
                     .ConfigureAwait(false);
 
@@ -23,11 +31,11 @@ namespace Atlas.API.Endpoints
                 {
                     return Results.Unauthorized();
                 }
-                
+              
                 IEnumerable<Log> logs = await supportData.GetLogsAsync(logArgs, cancellationToken)
-                    .ConfigureAwait(false);
+                .ConfigureAwait(false);
 
-                return Results.Ok(logs);
+                return Results.Ok(new AuthResult<IEnumerable<Log>?> { Authorisation = authorisation, Result = logs });
             }
             catch (AtlasException ex)
             {
