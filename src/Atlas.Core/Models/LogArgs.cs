296a1e1f9a97184a204f421ae4b0ffdca4663bf0
Grant Colley <grantcolley@yahoo.com>
2024-08-06 15:29:36 +0100

    Update LogArgs.cs

--- src/Atlas.Core/Models/LogArgs.cs
+++ src/Atlas.Core/Models/LogArgs.cs
@@ -4,10 +4,11 @@ namespace Atlas.Core.Models
 {
     public class LogArgs
     {
+        public string? Level { get; set; }
         public string? User { get; set; }
         public string? Message { get; set; }
         public string? Context { get; set; }
-        public DateTime? From { get; set; }
-        public DateTime? To { get; set; }
+        public DateTime? From { get; set; } = DateTime.Today;
+        public DateTime? To { get; set; } = DateTime.Now;
     }
 }
