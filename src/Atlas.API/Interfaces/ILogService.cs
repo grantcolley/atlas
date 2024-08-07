c5d2301f9ad886978c0b2c44db9ea8e7db7caa9b
Grant Colley <grantcolley@yahoo.com>
2024-08-07 11:13:33 +0100

    Update ILogService.cs

--- src/Atlas.API/Interfaces/ILogService.cs
+++ src/Atlas.API/Interfaces/ILogService.cs
@@ -4,6 +4,9 @@ namespace Atlas.API.Interfaces
 {
     public interface ILogService
     {
+        void Log(AtlasException? exception, string? user);
+        void Log(Enums.LogLevel logLevel, string? message, string? user);
+        void Log(Enums.LogLevel logLevel, AtlasException? exception, string? user);
         void Log(Enums.LogLevel logLevel, string? message, AtlasException? exception, string? user);
     }
 }
