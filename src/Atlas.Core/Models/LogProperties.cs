bfb3bef89f2ec008ae7c240f0be98ffff1ae19a7
Grant Colley <grantcolley@yahoo.com>
2024-08-08 19:55:52 +0100

    Create LogProperties.cs

---
+++ src/Atlas.Core/Models/LogProperties.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Atlas.Core.Models
+{
+    [XmlRoot("properties")]
+    public class LogProperties
+    {
+        public LogProperties() 
+        {
+            Properties = [];
+        }
+
+        [XmlElement("property")]
+        public List<LogProperty>? Properties { get; set; }
+    }
+}
