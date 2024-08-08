ab9e5404bcc257c6f83fb17efe0b0fd5c4d075fc
Grant Colley <grantcolley@yahoo.com>
2024-08-08 19:55:34 +0100

    Update IAtlasDialogService.cs

--- src/Atlas.Blazor.Web/Interfaces/IAtlasDialogService.cs
+++ src/Atlas.Blazor.Web/Interfaces/IAtlasDialogService.cs
@@ -1,9 +1,11 @@
 ﻿using Atlas.Blazor.Web.Models;
+using Atlas.Core.Models;
 
 namespace Atlas.Blazor.Web.Interfaces
 {
     public interface IAtlasDialogService
     {
+        Task ShowLogDialogAsync(Log log);
         Task<AtlasDialogContent?> ShowDialogAsync(string title, string message);
         Task<AtlasDialogContent?> ShowDialogAsync(string title, string message, AtlasDialogType dialogType);
         Task<AtlasDialogContent?> ShowDialogAsync(string title, List<string> messages, AtlasDialogType dialogType);
