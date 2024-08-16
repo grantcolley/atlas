f3ec796d6397a7a47bfa7b72826c6636333e4f19
Grant Colley <grantcolley@yahoo.com>
2024-08-16 21:29:34 +0100

    Update Auth.cs

--- src/Atlas.Core/Constants/Auth.cs
+++ src/Atlas.Core/Constants/Auth.cs
@@ -5,7 +5,6 @@
         public const string ATLAS_USER_CLAIM = "atlas-user";
         public const string ADMIN_READ = "Admin-Read";
         public const string ADMIN_WRITE = "Admin-Write";
-        public const string USER = "User";
         public const string DEVELOPER = "Developer";
         public const string SUPPORT = "Support";
     }
