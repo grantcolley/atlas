05752fcdc0d7d56d231a3509bfe2c2e4f96c3a0a
Grant Colley <grantcolley@yahoo.com>
2025-03-31 13:48:45 +0100

    Update AtlasConfig.cs

--- src/Atlas.Core/Models/AtlasConfig.cs
+++ src/Atlas.Core/Models/AtlasConfig.cs
@@ -2,6 +2,7 @@
 {
     public class AtlasConfig
     {
+        public bool DatabaseMigrate { get; set; }
         public bool DatabaseCreate { get; set; }
         public bool DatabaseSeedData { get; set; }
         public bool DatabaseSeedLogs { get; set; }
